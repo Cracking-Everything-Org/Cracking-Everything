002ce1f39e2b065a2543506da7e21932d9be705d
Pablo Pereira <invict1@outlook.es>
2019-07-27 17:25:30 -0300

    [CTCI] Linked Lists

---
+++ CTCI/Linked Lists/6.cpp
@@ -0,0 +1,43 @@
+using namespace std;
+#include <iostream> 
+#include <string>
+#include <stack>
+
+typedef int DataType;
+
+struct Node 
+{
+    Node* next = NULL;
+    DataType data;
+
+    Node(DataType data){
+        data=data;
+        next=NULL;
+    }
+};
+
+bool palindrome(Node* word){
+    stack<int> stk = {};
+    Node* faster = word;
+    Node* slower = word;
+    while(faster && faster->next){
+        stk.push(slower->data);
+        faster = faster->next->next;
+        slower = slower->next;
+    }
+    
+    //odd length
+    if(faster){
+        slower = slower->next;
+    }
+
+    while(slower){
+        int top = stk.top();
+        stk.pop();
+        if(top != slower->data){
+            return false;
+        }
+        slower=slower->next;
+    }
+    return true;
+}
