80a1a3a83544157afa98e7ac57c7ee622a7510de
Pablo Pereira <invict1@outlook.es>
2019-07-27 01:37:31 -0300

    [CTCI] Arrays-Strings

--- CTCI/Arrays - Strings/9.cpp
+++ CTCI/Arrays - Strings/9.cpp
@@ -2,6 +2,15 @@
 using namespace std; 
 
 bool stringRotation(string &s1, string s2){
+    int len= s1.length();
+    if(len == s2.length() && len>0){
+        string s1s1 = s1 + s1;
+        return isSubstring(s1s1,s2);
+    }
+    return false;
+}
+
+bool stringRotation(string &s1, string s2){
     if(s1.length()!=s2.length()) return false;
     int chances = s1.length();
     while(chances>0){
