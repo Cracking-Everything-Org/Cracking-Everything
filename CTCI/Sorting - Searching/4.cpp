c1af447128a632229e2e8f20ea55cffd710ac61a
Pablo Pereira <invict1@outlook.es>
2019-07-30 17:59:33 -0300

    [CTCI] Sorting-Searching

---
+++ CTCI/Sorting - Searching/4.cpp
@@ -0,0 +1,26 @@
+using namespace std;
+#include <iostream> 
+#include <string>
+#include <vector>
+
+int sortedSearchNoSize(Listy list, int value){
+    int index = 1;
+    while (list.elementAt(index)!= -1 && list.elementAt(index)<value) index*=2;
+
+    return binarySearch(list, value, index/2, index);
+}
+
+int binarySearch(Listy list, int value, int low, int high){
+    int mid;
+    while(low<high){
+        mid = low+(high-low)>>1;
+        if(list.elementAt(mid)<value){
+            low = mid++;
+        } else if(list.elementAt(high)>value){
+            high = mid--;
+        } else{
+            return mid;
+        }
+    }
+    return -1;
+}
