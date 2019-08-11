f35fef3cb6a7ae3a760edee91669ede5b93e2996
Pablo Pereira <invict1@outlook.es>
2019-08-11 20:08:14 -0300

    [CTCI] Recursion-DP

--- CTCI/Recursion - DP/9.cpp
+++ CTCI/Recursion - DP/9.cpp
@@ -1,13 +1,34 @@
 using namespace std;
 #include <string>
+#include <vector>
 #include <unordered_set>
 
-unordered_set<string> generateParens(int remaining){
+void addParen(vector<string> list, int leftRem, int rightRem, string str, int index){
+    if(leftRem < 0 || rightRem < leftRem) return;
+
+    if(leftRem == 0 && rightRem ==0){
+        list.push_back(str);
+    } else{
+        str[index] = '(';
+        addParen(list, leftRem-1 , rightRem, str, index+1);
+        
+        str[index] = ')';
+        addParen(list, leftRem , rightRem-1, str, index+1);
+    }
+}
+
+vector<string> generateParens(int remaining){
+    vector<string> result;
+    addParen(result, remaining, remaining, "", 0);
+    return result;
+}
+
+unordered_set<string> generateParens2(int remaining){
     unordered_set<string> set;
     if(remaining == 0){
         set.insert("");
     }else{
-        unordered_set prev = generateParens(remaining-1);
+        unordered_set prev = generateParens2(remaining-1);
         for(string str : prev){
             for(int i=0; i< str.length(); i++){
                 if(str[i] == '('){
