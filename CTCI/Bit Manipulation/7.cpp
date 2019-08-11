221629829469e2e5dfc6ecd7397ec7b12c7e3b92
Pablo Pereira <invict1@outlook.es>
2019-08-10 21:45:25 -0300

    no message

--- CTCI/Bit Manipulation/7.cpp
+++ CTCI/Bit Manipulation/7.cpp
@@ -2,5 +2,5 @@ using namespace std;
 #include <algorithm>
 
 int swapOddEvenBits(int x){
-    return (((x & 0xaaaaaaaa) >> 1) | ((x & 0x55555555) << 1));
+    return (((x & 0xaaaaaaaa) >> 1) | ((x & 0x55555555) << 1)); 
 }
