d8150ea2bad401923e99c44746645e3a5bc4da7f
Pablo Pereira <invict1@outlook.es>
2019-08-14 00:17:01 -0300

    UVa exercise

--- UVa/CHAPTER 1/11172.cpp
+++ UVa/CHAPTER 1/11172.cpp
@@ -5,14 +5,13 @@ using namespace std;
 void main () {
     int k, a, b;
     int c = 1;
-    while(scanf("%d", &k) != EOF){
-        while(k--){
-            if(c>1) printf("\n");
-            scanf("%d %d", &a, &b);
-            if (a < b) printf("<");
-            else if (a > b) printf(">");
-            else printf("=");
-            c++; 
-        }
-    }
+    scanf("%d", &k);
+    while(k--){
+        if(c>1) printf("\n");
+        scanf("%d %d", &a, &b);
+        if (a < b) printf("<");
+        else if (a > b) printf(">");
+        else printf("=");
+        c++; 
+    }   
 }
