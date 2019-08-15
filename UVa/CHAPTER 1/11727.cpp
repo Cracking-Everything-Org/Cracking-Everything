05f999664cbe3700ae2bd0cec0d49f9eed510987
Pablo Pereira <invict1@outlook.es>
2019-08-14 23:57:34 -0300

    UVa exercise

---
+++ UVa/CHAPTER 1/11727.cpp
@@ -0,0 +1,17 @@
+using namespace std;
+#include <iostream>
+#include <cmath>
+
+int lines,a,b,c;
+int i=1;
+scanf("%d",&lines);
+while(lines--){
+    scanf("%d",&a);
+    scanf("%d",&b);
+    scanf("%d",&c);
+    int preMax = max(b,c);
+    if(a>b && a>c) printf("Case %d: %d\n",i,max(b,c));
+    else if(b>a && b>c) printf("Case %d: %d\n",i,max(a,c));
+    else if(c>a && c>b) printf("Case %d: %d\n",i,max(a,b));
+    i++;
+}
