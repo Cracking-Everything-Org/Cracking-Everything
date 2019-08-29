e9de6e4cdc91a954efe8b7a5f441169656a1eaef
Pablo Pereira <invict1@outlook.es>
2019-08-28 22:01:41 -0300

    UVa exercise

--- UVa/UVa 1.3/11559.cpp
+++ UVa/UVa 1.3/11559.cpp
@@ -1,25 +1,58 @@
 #include <iostream>
-#include <cstdio>
+#include <fstream>
 #include <string>
+#include <vector>
 #include <algorithm>
+#include <cmath>
+#include <set>
+#include <map>
+ 
 using namespace std;
-
-int main () {
-    int n, b, h, w, hCost, hRooms, best;
-    while(scanf("%d %d %d %d\n", &n, &b, &h, &w) != EOF){
-        best = 5000001;
-        while(h--){
-            cin >> hCost;
-            for(int i=0;i<w;i++) {
-                cin >> hRooms;
-                if(hRooms >= n){
-                    best = min(best, hCost*n);
-                }
+ 
+#define FOR(i, a, b) for(ll i=ll(a); i<ll(b); i++)
+#define ROF(i, a, b) for(ll i=ll(a); i>=ll(b); i--)
+#define MAX 1000000000
+#define MIN -1000000000
+#define endl '\n'
+ 
+typedef long long ll;
+typedef pair<ll, ll> ii;
+typedef vector<ll> vi;
+typedef vector<string> vs;
+typedef vector<vi> mat;
+ 
+int tc, a, b, c, from, to;
+ 
+void doMov(int playerNum, int dieNum, vi fromTo, vi playerPos) {
+    int newPos = playerPos[playerNum]+dieNum;
+    playerPos[playerNum] = (fromTo[newPos] > 0)? fromTo[newPos] : newPos;
+}
+ 
+int main() {
+    cin >> tc;
+    while (tc--){
+        vi hm;
+        cin >> a >> b >> c;
+        vi playerPos;
+        int i = 0;
+        int cantP = a;
+        while (a--) {
+            playerPos[i] = 1;
+            i++;
+        }
+        while(b--) {
+            cin >> from >> to;
+            hm[from] = to;
+        }
+        int round = 1;
+        while(c--) {
+            if(playerPos[round % cantP] != 100) {
+                doMov(round % cantP, c, hm, playerPos);
             }
         }
-        if(best <= b) cout << best << endl;
-        else cout << "stay home" << endl;
+        FOR(i, 1, cantP) {
+            cout << "Position of player " << i << " is " << playerPos[i] << "." << endl;
+        }
     }
     return 0;
 }
-
