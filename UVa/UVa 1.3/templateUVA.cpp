0706404fc72ef2c6fdf1001d92530605d54df3e7
Pablo Pereira <invict1@outlook.es>
2019-08-29 22:29:40 -0300

    UVa exercise

---
+++ UVa/UVa 1.3/templateUVA.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <fstream>
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+#define FOR(i, a, b) for(ll i=ll(a); i<ll(b); i++)
+#define ROF(i, a, b) for(ll i=ll(a); i>=ll(b); i--)
+#define MAX 1000000000
+#define MIN -1000000000
+#define endl "\n"
+
+typedef long long ll;
+typedef pair<ll, ll> ii;
+typedef vector<ll> vi;
+typedef vector<string> vs;
+typedef vector<vi> mat;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	return 0;
+}
