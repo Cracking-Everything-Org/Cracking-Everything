8a4c32306c132c7e1ea14805101240eec550df0d
Pablo Pereira <invict1@outlook.es>
2019-12-14 00:18:14 -0300

    no message

---
+++ Udemy Course/Big O/2.js
@@ -0,0 +1,19 @@
+// What is the Big O of the below function? (Hint, you may want to go line by line)
+function anotherFunChallenge(input) {
+  let a = 5;
+  let b = 10;
+  let c = 50;
+  for (let i = 0; i < input; i++) {
+    let x = i + 1;
+    let y = i + 2;
+    let z = i + 3;
+
+  }
+  for (let j = 0; j < input; j++) {
+    let p = j * 2;
+    let q = j * 2;
+  }
+  let whoAmI = "I don't know";
+}
+
+// O(n)
