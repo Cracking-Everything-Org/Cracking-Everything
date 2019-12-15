ef2c01ac18061f49f3ea3e1424ed6e51135383df
Pablo Pereira <invict1@outlook.es>
2019-12-15 17:27:12 -0300

    no message

---
+++ LeetCode/217.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> hs;
+        for(int i = 0; i< nums.size(); i++){
+            if(hs.find(nums[i])!=hs.end()) return true;
+            else hs.insert(nums[i]);
+        }
+        return false;
+    }
+};
