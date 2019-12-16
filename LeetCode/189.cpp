a3816929fe3382da70e1c0c8fa0a447e6c498402
Pablo Pereira <invict1@outlook.es>
2019-12-16 05:48:17 -0300

    no message

---
+++ LeetCode/189.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int> numsCopy(nums);
+
+        for(int i = 0; i < n; i++)
+            nums[(i+k)%n] = numsCopy[i];
+    }
+};
