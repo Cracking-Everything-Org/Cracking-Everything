3f6f5cd3b6f68efee2af4ebba9fdf4a19a7209ea
Pablo Pereira <invict1@outlook.es>
2019-12-25 13:55:10 -0300

    no message

---
+++ LeetCode/169.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        unordered_map<int, int> ht;
+        for(int i=0; i<nums.size(); i++){
+            if(ht.find(nums[i]) != ht.end()){
+                ht.insert({nums[i], 1});
+            }else {
+                ht.insert({nums[i], nums[i]++});
+            }
+        }
+        for (auto it : ht) {
+            if (it.second > (nums.size() / 2)) {
+                return it.first;
+            }
+        }
+
+        return -1;
+    }
+};
+
+// también pensé en ordenarlo y retornar el elemento del medio
