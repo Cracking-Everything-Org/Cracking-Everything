dd358fa9b47ee8d21756a665288fc2c976568c2f
Pablo Pereira <invict1@outlook.es>
2019-12-26 16:54:58 -0300

    no message

---
+++ LeetCode/21.cpp
@@ -0,0 +1,16 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* a, ListNode* b) {
+        if (!a || b && a->val > b->val) swap(a, b);
+        if (a) a->next = mergeTwoLists(a->next, b);
+        return a;
+    }
+};
