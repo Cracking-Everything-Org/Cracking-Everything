8d300b96ae8c568aff48eab2e95dff1dd254b1da
Pablo Pereira <invict1@outlook.es>
2019-12-28 23:53:03 -0300

    no message

---
+++ LeetCode/680.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool validPalindrome(string s) {
+        int lo = 0, hi = s.size() - 1;
+        return validPalindrome(s, lo, hi, 0);
+    }
+
+    bool validPalindrome(string &s, int lo, int hi, int count) {
+        if (count > 1) return false;
+        while (lo < hi) {
+            if (s[lo] == s[hi]) {
+                lo++; hi--;
+            }
+            else {
+                return validPalindrome(s, lo + 1, hi, count + 1) ||
+                        validPalindrome(s, lo, hi - 1, count + 1);
+            }
+        }
+        return true;
+    }
+};
