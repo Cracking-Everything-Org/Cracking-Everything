0274f89d0a90b86153f2a005527ee42fce7bb25d
Pablo Pereira <invict1@outlook.es>
2019-12-28 21:23:37 -0300

    no message

---
+++ LeetCode/389.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        int sumS = 0;
+        for(int i = 0; i< s.length(); i++) sumS -= s[i];
+        for(int j = 0; j< t.length(); j++) sumS += t[j];
+        return (char)sumS;
+    }
+};
