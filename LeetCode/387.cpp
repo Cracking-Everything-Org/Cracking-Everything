d9f2176a31ec48375070460038f398c8b025df74
Pablo Pereira <invict1@outlook.es>
2019-12-30 12:13:00 -0300

    no message

--- LeetCode/387.cpp
+++ LeetCode/387.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     int firstUniqChar(string s) {
         unordered_map<char,int> ht;
-        for (int i=0; i< s.size(); i++) {
-            if (ht.find(s[i]) != ht.end()) {
-                ht[s[i]]++;
-            } else {
-                ht.insert({s[i],1});
+        for(auto c : s){
+            if(ht.find(c) == ht.end()){
+                ht[c] = 1;
+            }else {
+                ht[c]++;
             }
         }
-        for (int i=0; i< s.size(); i++) {
-            if (ht[s[i]] == 1) return i;
+        for(int i=0; i<s.length(); i++){
+            if(ht[s[i]]==1) return i;
         }
         return -1;
     }
