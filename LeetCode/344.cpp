4fe44d93a0fbd72eb8ac8eb339f9c239205926e7
Pablo Pereira <invict1@outlook.es>
2019-12-30 12:16:58 -0300

    no message

--- LeetCode/344.cpp
+++ LeetCode/344.cpp
@@ -1,13 +1,6 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        char cup;
-        int sLength = s.size();
-        if(sLength == 0) return;
-        for (int i=0; i<=(sLength-1)/2; i++) {
-            cup = s[i];
-            s[i] = s[sLength-i-1];
-            s[sLength-i-1] = cup;
-        }
+        for(int i=0; i<s.size()/2; i++) swap(s[i], s[s.size()-1-i]);
     }
 };
