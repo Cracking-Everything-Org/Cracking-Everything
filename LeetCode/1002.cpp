33313e508cdb4c7367c8875da431bc4eb2e677fa
Pablo Pereira <invict1@outlook.es>
2019-12-31 19:34:41 -0300

    no message

---
+++ LeetCode/1002.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    void combineHT(unordered_map<char, int>& htFinal,unordered_map<char, int>& htTurn){
+        if(htFinal.size()==0) {
+            htFinal = htTurn;
+            return ;
+        }
+        vector<char> toErase;
+        for(auto& it: htFinal){
+            if(htTurn.find(it.first) != htTurn.end()){
+                htFinal[it.first] = min(it.second, htTurn[it.first]);
+            } else {
+                toErase.push_back(it.first);
+            }
+        }
+        for(auto ch: toErase) htFinal.erase(ch);
+    }
+
+    vector<string> commonChars(vector<string>& A) {
+        unordered_map<char,int> htFinal;
+        vector<string> result;
+        for(auto str : A){
+            unordered_map<char,int> htTurn;
+            for(int i = 0; i< str.length(); i++){
+                if(htTurn.find(str[i]) != htTurn.end()){
+                    htTurn[str[i]]++;
+                } else {
+                    htTurn[str[i]] = 1;
+                }
+            }
+            combineHT(htFinal, htTurn);
+        }
+        for(auto& it: htFinal){
+            while(it.second > 0) {
+                char num = it.first;
+                result.push_back(to_string(num));
+                htFinal[it.first]--;
+            }
+        }
+        return result;
+    }
+};
