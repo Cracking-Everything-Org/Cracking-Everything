032314edfe260f121c911497572643db308b3a45
Pablo Pereira <invict1@outlook.es>
2019-12-31 13:24:03 -0300

    no message

---
+++ LeetCode/1295.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int findNumbers(vector<int>& nums) {
+        int counter = 0;
+        for(auto i : nums){
+            if(to_string(i).length()%2==0) counter++;
+        }
+        return counter;
+    }
+};
