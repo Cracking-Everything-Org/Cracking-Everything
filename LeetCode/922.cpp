70734f757177f74f245bed3e5448c77dfa24398b
Pablo Pereira <invict1@outlook.es>
2019-12-31 19:34:57 -0300

    no message

---
+++ LeetCode/922.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> sortArrayByParityII(vector<int>& A) {
+        int current = 0;
+        int last = A.size()-1;
+        while(current < A.size()){
+            if(current %2 != 0){
+                while(A[last] %2 == 0) last--;
+                swap(A[current],A[last]);
+            } else {
+                while(A[last] %2 != 0) last--;
+                swap(A[current],A[last]);
+            }
+            last = A.size()-1;
+            current++;
+        }
+        return A;
+    }
+};
