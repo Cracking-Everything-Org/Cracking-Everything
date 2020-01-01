8fcbdc4e73b526b268593db4a962f18185ddb7c0
Pablo Pereira <invict1@outlook.es>
2020-01-01 19:43:42 -0300

    no message

---
+++ LeetCode/203.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        while(head && head->val == val) head=head->next;
+        ListNode* start = head;
+        while(head && head->next){
+            if(head->next->val == val){
+                head->next = head->next->next;
+            }
+            else head = head->next;
+        }
+        return start;
+    }
+};
