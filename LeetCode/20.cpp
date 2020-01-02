7175b6042b5d9f7ca847b2acfae385c1c078269f
Pablo Pereira <invict1@outlook.es>
2020-01-02 11:21:59 -0300

    no message

--- LeetCode/20.cpp
+++ LeetCode/20.cpp
@@ -2,33 +2,23 @@ class Solution {
 public:
     bool isValid(string s) {
         stack<char> stk;
-        for(char c : s){
-            if( c=='(' || c== '{' || c== '['){
-                stk.push(c);
-            } else{
-                if(stk.empty()) return false;
-                if( c== ')' ){
-                    if(stk.top()=='('){
-                        stk.pop();
-                    } else {
-                        return false;
-                    }
-                } else if ( c== '}' ){
-                    if(stk.top()=='{'){
-                        stk.pop();
-                    } else {
-                        return false;
-                    }
-                } else {
-                    if(stk.top()=='['){
-                        stk.pop();
-                    } else {
-                        return false;
-                    }
-                }
+        for (int i=0; i<s.size(); i++) {
+            if (s[i] == ']') {
+                if (stk.empty()) return false;
+                if (stk.top() != '[' ) return false;
+                stk.pop();
+            } else if (s[i] == ')'){
+                if (stk.empty()) return false;
+                if (stk.top()!= '(') return false;
+                stk.pop();
+            } else if (s[i] == '}') {
+                if (stk.empty()) return false;
+                if (stk.top()!= '{') return false;
+                stk.pop();
+            } else {
+                stk.push(s[i]);
             }
         }
-        if(!stk.empty()) return false;
-        return true;
+        return stk.empty();
     }
 };
