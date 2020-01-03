b4165b8b14fb70ece10421106e025cce84d57714
Pablo Pereira <invict1@outlook.es>
2020-01-03 13:09:29 -0300

    no message

---
+++ LeetCode/278.cpp
@@ -0,0 +1,17 @@
+// Forward declaration of isBadVersion API.
+bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int left = 1;
+        int right = n;
+        int mid;
+        while(left < right) {
+            mid = left + (right-left)/2;
+            if (isBadVersion(mid)) right = mid;
+            else left = mid + 1;
+        }
+        return left;
+    }
+};
