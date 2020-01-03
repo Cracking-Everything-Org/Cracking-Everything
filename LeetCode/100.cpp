5ef139e20153c23e31729a90a0eeda7f4a39cd37
Pablo Pereira <invict1@outlook.es>
2020-01-03 16:29:12 -0300

    no message

--- LeetCode/100.cpp
+++ LeetCode/100.cpp
@@ -12,8 +12,8 @@ public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
         if(!p && !q) return true;
         if(!p || !q) return false;
-        if(p->val == q->val){
-            return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
-        }else return false;
+        return p->val == q->val
+            && isSameTree(p->left, q->left)
+            && isSameTree(p->right, q->right);
     }
 };
