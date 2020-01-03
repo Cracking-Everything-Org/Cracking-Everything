9f88b1c4e4ced18ae89d74ea2303e8768b0068ff
Pablo Pereira <invict1@outlook.es>
2020-01-03 16:10:25 -0300

    no message

--- LeetCode/104.cpp
+++ LeetCode/104.cpp
@@ -10,10 +10,7 @@
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
-        if(!root){
-            return 0;
-        } else {
-            return 1 + max(maxDepth(root->left), maxDepth(root->right));
-        }
+        if(!root) return 0;
+        return 1 + max(maxDepth(root->left), maxDepth(root->right));
     }
 };
