e92db303c801b1d27dc9c72db28a996538f2e608
Pablo Pereira <invict1@outlook.es>
2020-01-16 06:45:04 -0300

    no message

--- LeetCode/1.cpp
+++ LeetCode/1.cpp
@@ -4,7 +4,7 @@ public:
         unordered_map<int,int> ht;
         vector<int> result;
         for(int i=0; i< nums.size(); i++){
-            if(ht.find(target-nums[i]) != ht.end()){
+            if(ht.find(target-nums[i]) != ht.end()) {
                 result.push_back(i);
                 result.push_back(ht.find(target-nums[i])->second);
             }else ht[nums[i]]=i;
