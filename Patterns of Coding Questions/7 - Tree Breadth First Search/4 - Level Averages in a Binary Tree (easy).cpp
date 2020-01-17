01fa2df642283675a3895b775100de4a87786629
Pablo Pereira <invict1@outlook.es>
2020-01-17 20:47:30 -0300

    no message

--- Patterns of Coding Questions/7 - Tree Breadth First Search/4 - Level Averages in a Binary Tree (easy).cpp
+++ Patterns of Coding Questions/7 - Tree Breadth First Search/4 - Level Averages in a Binary Tree (easy).cpp
@@ -21,7 +21,7 @@ vector<double> findLevelAverages(TreeNode *root) {
   }
   return result;
 }
-
+ 
 Time complexity #
 The time complexity of the above algorithm is O(N), where ‘N’ is the total number of nodes in the tree. This is due to the fact that we traverse each node once.
 
