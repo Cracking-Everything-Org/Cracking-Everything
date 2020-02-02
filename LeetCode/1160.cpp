634450a072b78497ec8ddae9c13b4c9c61c3c5f4
Pablo Pereira <invict1@outlook.es>
2020-02-02 20:27:49 -0300

    no message

--- LeetCode/1160.cpp
+++ LeetCode/1160.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool goodString(unordered_map<char, int> iterationHT, unordered_map<char, int> ht){
+    bool goodString(unordered_map<char, int> iterationHT, unordered_map<char, int> ht) {
         for(auto it: iterationHT){
             if(ht.find(it.first)== ht.end()) return false;
             if(ht[it.first] < it.second) return false;
