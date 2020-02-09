54a7ec732aefd85a2afa4eecdefcecd1b87bc74d
Pablo Pereira <invict1@outlook.es>
2020-02-09 20:34:41 -0300

    no message

--- LeetCode/922.cpp
+++ LeetCode/922.cpp
@@ -3,7 +3,7 @@ public:
     vector<int> sortArrayByParityII(vector<int>& A) {
         int current = 0;
         int last = A.size()-1;
-        while(current < A.size()){
+        while(current < A.size()) {
             if(current %2 != 0){
                 while(A[last] %2 == 0) last--;
                 swap(A[current],A[last]);
