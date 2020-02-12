66837e2b009b33f0ef3d555b724a06832fea22bd
Pablo Pereira <invict1@outlook.es>
2020-02-11 23:03:17 -0300

    no message

--- LeetCode/977.cpp
+++ LeetCode/977.cpp
@@ -3,7 +3,7 @@ public:
     vector<int> sortedSquares(vector<int>& A) {
         for(int i=0; i< A.size();i++){
             A[i] = pow(A[i],2);
-        }
+        } 
         sort(A.begin(),A.end());
         return A;
     }
