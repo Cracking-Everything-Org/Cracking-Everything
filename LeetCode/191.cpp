5aadb0f16cdfeb7f16dc7b9de1be3ba8c8589bcd
Pablo Pereira <invict1@outlook.es>
2020-02-24 23:00:04 -0300

    no message

--- LeetCode/191.cpp
+++ LeetCode/191.cpp
@@ -3,8 +3,8 @@ public:
     int hammingWeight(uint32_t n) {
         int bits=0;
         uint32_t mask = 1;
-        for(int i=0; i<32; i++){
-            if(n&mask){
+        for(int i=0; i<32; i++) {
+            if(n&mask) {
                 bits++;
             }
             mask = mask << 1;
