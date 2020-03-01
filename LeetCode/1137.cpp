7c1e54ad9836f6be3c493e60e4848f3882b429a9
Pablo Pereira <invict1@outlook.es>
2020-02-29 21:15:59 -0300

    no message

--- LeetCode/1137.cpp
+++ LeetCode/1137.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
 
-    unordered_map<int, int>hs;
+    unordered_map<int, int> hs;
     int tribonacci(int n) {
         if(n<2) return n;
         if(n==2) return 1;
@@ -9,6 +9,6 @@ public:
         if(hs.find(n-1)==hs.end()) hs[n-1]=tribonacci(n-1);
         if(hs.find(n-2)==hs.end()) hs[n-2]=tribonacci(n-2);
         if(hs.find(n-3)==hs.end()) hs[n-3]=tribonacci(n-3);
-        return hs[n-1]+hs[n-2]+hs[n-3];   
+        return hs[n-1]+hs[n-2]+hs[n-3];
     }
 };
