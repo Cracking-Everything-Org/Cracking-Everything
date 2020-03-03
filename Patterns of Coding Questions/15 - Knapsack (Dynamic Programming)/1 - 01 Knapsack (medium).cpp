91f9fe92def71d0e3cfee6011dbfc3e5141f2c87
Pablo Pereira <invict1@outlook.es>
2020-03-02 21:52:41 -0300

    no message

---
+++ Patterns of Coding Questions/15 - Knapsack (Dynamic Programming)/1 - 01 Knapsack (medium).cpp
@@ -0,0 +1,94 @@
+Given two integer arrays to represent weights and profits of ‘N’ items, we need to find a subset of these items which will give us maximum profit such that their cumulative weight is not more than a given number ‘C’. Each item can only be selected once, which means either we put an item in the knapsack or we skip it.
+
+
+class Knapsack {
+ public:
+  int solveKnapsack(const vector<int> &profits, vector<int> &weights, int capacity) {
+    // basic checks
+    if (capacity <= 0 || profits.empty() || weights.size() != profits.size()) {
+      return 0;
+    }
+
+    int n = profits.size();
+    vector<vector<int>> dp(n, vector<int>(capacity + 1));
+
+    // populate the capacity=0 columns, with '0' capacity we have '0' profit
+    for (int i = 0; i < n; i++) {
+      dp[i][0] = 0;
+    }
+
+    // if we have only one weight, we will take it if it is not more than the capacity
+    for (int c = 0; c <= capacity; c++) {
+      if (weights[0] <= c) {
+        dp[0][c] = profits[0];
+      }
+    }
+
+    // process all sub-arrays for all the capacities
+    for (int i = 1; i < n; i++) {
+      for (int c = 1; c <= capacity; c++) {
+        int profit1 = 0, profit2 = 0;
+        // include the item, if it is not more than the capacity
+        if (weights[i] <= c) {
+          profit1 = profits[i] + dp[i - 1][c - weights[i]];
+        }
+        // exclude the item
+        profit2 = dp[i - 1][c];
+        // take maximum
+        dp[i][c] = max(profit1, profit2);
+      }
+    }
+
+    // maximum profit will be at the bottom-right corner.
+    return dp[n - 1][capacity];
+  }
+};
+
+
+int solveKnapsack(const vector<int> &profits, vector<int> &weights, int capacity) {
+    // base checks
+    if (capacity <= 0 || profits.empty() || weights.size() != profits.size()) {
+      return 0;
+    }
+
+    int n = profits.size();
+    vector<vector<int>> dp(n, vector<int>(capacity + 1));
+
+    // populate the capacity=0 columns, with '0' capacity we have '0' profit
+    for (int i = 0; i < n; i++) {
+      dp[i][0] = 0;
+    }
+
+    // if we have only one weight, we will take it if it is not more than the
+    // capacity
+    for (int c = 0; c <= capacity; c++) {
+      if (weights[0] <= c) {
+        dp[0][c] = profits[0];
+      }
+    }
+
+    // process all sub-arrays for all the capacities
+    for (int i = 1; i < n; i++) {
+      for (int c = 1; c <= capacity; c++) {
+        int profit1 = 0, profit2 = 0;
+        // include the item, if it is not more than the capacity
+        if (weights[i] <= c) {
+          profit1 = profits[i] + dp[i - 1][c - weights[i]];
+        }
+        // exclude the item
+        profit2 = dp[i - 1][c];
+        // take maximum
+        dp[i][c] = max(profit1, profit2);
+      }
+    }
+
+    printSelectedElements(dp, weights, profits, capacity);
+    // maximum profit will be at the bottom-right corner.
+    return dp[n - 1][capacity];
+  }
+
+
+Time and Space complexity #
+Since our memoization array dp[profits.length][capacity+1] stores the results for all subproblems, we can conclude that we will not have more than N*C subproblems (where ‘N’ is the number of items and ‘C’ is the knapsack capacity). This means that our time complexity will be O(N*C).
+
+The above algorithm will use O(N*C) space for the memoization array. Other than that we will use O(N) space for the recursion call-stack. So the total space complexity will be O(N*C + N), which is asymptotically equivalent to O(N*C).
