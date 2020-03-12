ea8089f53c81019baba375d780ea7d7fad43dac9
Pablo Pereira <invict1@outlook.es>
2020-03-11 21:17:32 -0300

    Update 1295.cpp

--- Exercises - LeetCode/1295.cpp
+++ Exercises - LeetCode/1295.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int findNumbers(vector<int>& nums) {
         int counter = 0;
-        for(auto i : nums){
+        for(auto i : nums) {
             if(to_string(i).length()%2==0) counter++;
         }
         return counter;
