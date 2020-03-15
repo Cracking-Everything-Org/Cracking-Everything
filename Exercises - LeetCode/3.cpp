aff4da3224843c72a3d6a7cbcaa03ccbf9d0a7e0
Pablo Pereira <invict1@outlook.es>
2020-03-14 23:11:00 -0300

    no message

---
+++ Exercises - LeetCode/3.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int maxLength = 0;
+        int currLength = 0;
+        int windowStart = 0;
+        int windowEnd;
+        unordered_map<char,int> ht;
+        for (windowEnd = 0; windowEnd < s.length(); windowEnd++) {
+            ht[s[windowEnd]] ++;
+            currLength ++;
+            while(ht[s[windowEnd]]>1) {
+                ht[s[windowStart]]--;
+                windowStart++;
+                currLength--;
+            }
+            maxLength = max(maxLength, currLength);
+        }
+        return maxLength;
+    }
+};
