6f77106b191cb16c8c46c3ea2f947ae23ec4edba
Pablo Pereira <invict1@outlook.es>
2020-03-15 23:12:23 -0300

    no message

---
+++ Exercises - LeetCode/560.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int count = 0;
+        for (int start = 0; start < nums.size(); start++) {
+            int sum = 0;
+            for (int end = start; end < nums.size(); end++) {
+                sum += nums[end];
+                if (sum == k) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+};
