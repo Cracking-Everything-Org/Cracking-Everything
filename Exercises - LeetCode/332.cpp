e1ae166ac13aa885ff7a860841896f4e5a812a2b
Pablo Pereira <invict1@outlook.es>
2020-03-15 23:12:14 -0300

    no message

---
+++ Exercises - LeetCode/332.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int mid = 0;
+        int low = 0;
+        int high = nums.size() - 1;
+        while(low <= high){
+            mid = low + ((high-low)/2);
+            if(target == nums[mid]){
+                return mid;
+            }else if(target < nums[mid]){
+                high = mid - 1;
+            }else{
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+};
