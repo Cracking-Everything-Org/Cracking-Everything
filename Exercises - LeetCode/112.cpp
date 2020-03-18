d97fbe5629893a6ed1169f6721ed62eb9f4440fe
Pablo Pereira <invict1@outlook.es>
2020-03-17 23:20:57 -0300

    no message

--- Exercises - LeetCode/112.cpp
+++ Exercises - LeetCode/112.cpp
@@ -10,11 +10,9 @@
 class Solution {
 public:
     bool hasPathSum(TreeNode* root, int sum) {
-        if(!root) return false;
-        if(root->val == sum
-           && !root->left
-           && !root->right) return true;
-        return hasPathSum(root->left, sum - root->val)
-            || hasPathSum(root->right, sum - root->val);
+        if (!root) return false;
+        sum -= root->val;
+        if (sum == 0 && !root->left && !root->right) return true;
+        return hasPathSum(root->left, sum) || hasPathSum(root->right, sum);
     }
 };
