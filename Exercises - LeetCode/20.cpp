7a00023485882d08a963f74f2987f563cbfd67df
Pablo Pereira <invict1@outlook.es>
2020-03-18 00:42:34 -0300

    no message

--- Exercises - LeetCode/20.cpp
+++ Exercises - LeetCode/20.cpp
@@ -2,23 +2,24 @@ class Solution {
 public:
     bool isValid(string s) {
         stack<char> stk;
-        for (int i=0; i<s.size(); i++) {
-            if (s[i] == ']') {
-                if (stk.empty()) return false;
-                if (stk.top() != '[' ) return false;
-                stk.pop();
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == '}') {
+                if (!stk.size()) return false;
+                if (stk.top() == '{') {
+                    stk.pop();
+                } else return false;
+            } else if (s[i] == ']') {
+                if (!stk.size()) return false;
+                if (stk.top() == '[') {
+                    stk.pop();
+                } else return false;
             } else if (s[i] == ')') {
-                if (stk.empty()) return false;
-                if (stk.top()!= '(') return false;
-                stk.pop();
-            } else if (s[i] == '}') {
-                if (stk.empty()) return false;
-                if (stk.top()!= '{') return false;
-                stk.pop();
-            } else {
-                stk.push(s[i]);
-            }
+                if (!stk.size()) return false;
+                if (stk.top() == '(') {
+                    stk.pop();
+                } else return false;
+            } else stk.push(s[i]);
         }
-        return stk.empty();
+        return stk.size() == 0;
     }
 };
