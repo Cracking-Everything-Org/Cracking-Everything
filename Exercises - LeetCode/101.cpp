9b2b2f0a19115608edb7c463c8997bda9ce6bb66
Pablo Pereira <invict1@outlook.es>
2020-03-23 21:14:33 -0300

    no message

--- Exercises - LeetCode/101.cpp
+++ Exercises - LeetCode/101.cpp
@@ -14,7 +14,7 @@ public:
         return isSym(root->left, root->right);
     }
 
-    bool isSym(TreeNode* r1, TreeNode* r2){
+    bool isSym(TreeNode* r1, TreeNode* r2) {
         if(!r1 && !r2) return true;
         if(!r1 || !r2) return false;
         if(r1->val != r2->val) return false;
