a777848e0f35a964c6d82aadd67ab412119fdff1
Pablo Pereira <invict1@outlook.es>
2020-03-25 23:19:51 -0300

    no message

--- Exercises - LeetCode/217.cpp
+++ Exercises - LeetCode/217.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
         unordered_set<int> hs;
-        for(int i = 0; i< nums.size(); i++){
-            if(hs.find(nums[i])!=hs.end()) return true;
+        for (int i = 0; i < nums.size(); i++) {
+            if (hs.find(nums[i]) != hs.end()) return true;
             else hs.insert(nums[i]);
         }
         return false;
