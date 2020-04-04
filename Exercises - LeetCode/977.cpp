6958e58cea13858c4397e34d3443200e7a446a70
Pablo Pereira <invict1@outlook.es>
2020-04-03 21:58:15 -0300

    no message

--- Exercises - LeetCode/977.cpp
+++ Exercises - LeetCode/977.cpp
@@ -1,10 +1,23 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& A) {
-        for(int i=0; i< A.size();i++){
-            A[i] = pow(A[i],2);
-        } 
-        sort(A.begin(),A.end());
-        return A;
+        vector<int> squares;
+        stack<int> stk;
+        int leftPointer = 0;
+        int rightPointer = A.size()-1;
+        while (leftPointer <= rightPointer) {
+            if (pow(A[leftPointer],2) >= pow(A[rightPointer],2)) {
+                stk.push(pow(A[leftPointer],2));
+                leftPointer++;
+            } else {
+                stk.push(pow(A[rightPointer],2));
+                rightPointer--;
+            }
+        }
+        while (!stk.empty()) {
+            squares.push_back(stk.top());
+            stk.pop();
+        }
+        return squares;
     }
 };
