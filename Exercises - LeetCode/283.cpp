6f826b65ad0ec7cd19acedb49780aee210db7961
Pablo Pereira <invict1@outlook.es>
2020-04-05 12:16:55 -0300

    no message

--- Exercises - LeetCode/283.cpp
+++ Exercises - LeetCode/283.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int count = 0;
-        int actual = 0;
-        for(int i = 0; i< nums.size(); i++){
-            if(nums[i]==0){
-                count++;
-            }else if(count!=0){
-                nums[i-count] = nums[i];
-                nums[i] = 0;
-            }
+        int a = 0;
+        int z = 0;
+        while (a < nums.size() && z < nums.size()) {
+            if (a >= z) {
+                swap(nums[a], nums[z]);
+                a++;
+            } else a++;
+            while (z < nums.size() && nums[z] != 0) z++;
+            while (a < nums.size() && nums[a] == 0) a++;
         }
     }
 };
