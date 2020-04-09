fcf68122b1ee62296df8d00374c310df6f465cb2
Pablo Pereira <invict1@outlook.es>
2020-04-09 20:17:17 -0300

    no message

--- Exercises - LeetCode/350.cpp
+++ Exercises - LeetCode/350.cpp
@@ -2,21 +2,22 @@ class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
         vector<int> intersection;
-        unordered_map<int, int> hash;
-
-        for (int i:nums1) {
-            if(hash.find(i) != hash.end()) {
-                hash[i]++;
-            }else{
-                hash[i] = 1;
+        int n1Pointer = 0;
+        int n2Pointer = 0;
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+        while (n1Pointer < nums1.size() && n2Pointer < nums2.size()) {
+            if (nums1[n1Pointer] == nums2[n2Pointer]) {
+                intersection.push_back(nums1[n1Pointer]);
+                n1Pointer++;
+                n2Pointer++;
+            } else {
+                if (nums1[n1Pointer] < nums2[n2Pointer]) {
+                    n1Pointer++;
+                } else {
+                    n2Pointer++;
+                }
             }
-        }
-        for (int i:nums2) {
-            if(hash.find(i) != hash.end()){
-                hash[i]--;
-                intersection.push_back(i);
-            }
-            if(hash[i]==0) hash.erase(i);
         }
         return intersection;
     }
