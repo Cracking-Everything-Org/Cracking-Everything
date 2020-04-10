703fb0221506fcbbf8fc89a89316f76575658431
Pablo Pereira <invict1@outlook.es>
2020-04-10 02:53:19 -0300

    no message

--- Exercises - LeetCode/121.cpp
+++ Exercises - LeetCode/121.cpp
@@ -3,8 +3,7 @@ public:
     int maxProfit(vector<int>& prices) {
         int profit = 0;
         int buy = INT_MAX;
-
-        for (int i = 0 ; i < prices.size(); i++) {
+        for (int i = 0; i < prices.size(); i++) {
             if (prices[i] < buy) {
                 buy = prices[i];
             } else {
