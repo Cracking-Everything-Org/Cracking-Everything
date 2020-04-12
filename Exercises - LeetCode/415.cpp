c79832792c1d4f0a856657c37de115be4b74fae2
Pablo Pereira <invict1@outlook.es>
2020-04-12 12:55:18 -0300

    no message

--- Exercises - LeetCode/415.cpp
+++ Exercises - LeetCode/415.cpp
@@ -1,13 +1,20 @@
 class Solution {
 public:
     string addStrings(string num1, string num2) {
-        long sumResult = 0;
-        for(int i=num1.length()-1; i>=0; i--){
-            sumResult += (num1[i]-'0')*(pow(10,num1.length()-1-i));
+        int i = num1.size() - 1;
+        int j = num2.size() - 1;
+        int carry = 0;
+        string res = "";
+        while(i>=0 || j>=0 || carry){
+            long sum = 0;
+            if(i >= 0){sum += (num1[i] - '0');i--;}
+            if(j >= 0){sum += (num2[j] - '0');j--;}
+            sum += carry;
+            carry = sum / 10;
+            sum = sum % 10;
+            res =  res + to_string(sum);
         }
-        for(int i=num2.length()-1; i>=0; i--){
-            sumResult += (num2[i]-'0')*(pow(10,num2.length()-1-i));
-        }
-        return to_string(sumResult);
+        reverse(res.begin(), res.end());
+        return res;
     }
 };
