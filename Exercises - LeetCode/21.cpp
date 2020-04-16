930368d8a8724c7f8698f7d0825e5222a50fb845
Pablo Pereira <invict1@outlook.es>
2020-04-15 21:10:32 -0300

    no message

--- Exercises - LeetCode/21.cpp
+++ Exercises - LeetCode/21.cpp
@@ -29,3 +29,48 @@ public:
         return cur;
     }
 };
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode* start = new ListNode(-1);
+        ListNode* current = start;
+
+        ListNode* flPointer = l1;
+        ListNode* slPointer = l2;
+        while (flPointer != NULL && slPointer != NULL) {
+            if (flPointer->val <= slPointer->val) {
+                ListNode* newNode = new ListNode(flPointer->val);
+                current->next = newNode;
+                current = newNode;
+                flPointer = flPointer->next;
+            } else {
+                ListNode* newNode = new ListNode(slPointer->val);
+                current->next = newNode;
+                current = newNode;
+                slPointer = slPointer->next;
+            }
+        }
+        while (flPointer != NULL) {
+            ListNode* newNode = new ListNode(flPointer->val);
+            current->next = newNode;
+            current = newNode;
+            flPointer = flPointer->next;
+        }
+        while (slPointer != NULL) {
+            ListNode* newNode = new ListNode(slPointer->val);
+            current->next = newNode;
+            current = newNode;
+            slPointer = slPointer->next;
+        }
+        return start->next;
+    }
+};
