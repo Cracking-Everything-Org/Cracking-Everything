c92ef1803104d34c4c9342e37efee5dd5b5170c2
Pablo Pereira <invict1@outlook.es>
2020-04-18 04:06:30 -0300

    no message

---
+++ Exercises - LeetCode/1004.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int longestOnes(vector<int>& A, int K) {
+        int longestLength = 0;
+        int currentLength = 0;
+        int startWindow = 0;
+        int zerosFound = 0;
+        for (int endWindow = 0; endWindow < A.size(); endWindow++) {
+            currentLength++;
+            if (A[endWindow] == 0) {
+                zerosFound++;
+                while(zerosFound > K) {
+                    if(A[startWindow] == 0) {
+                        zerosFound--;
+                    }
+                    startWindow++;
+                    currentLength--;
+                }
+            }
+            longestLength = max(longestLength,max(currentLength, endWindow - startWindow + 1));
+        }
+        return longestLength;
+    }
+};
