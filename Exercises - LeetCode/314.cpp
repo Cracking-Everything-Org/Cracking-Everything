91d5f0e68fc175392415d1ee4f9f081a6def6711
Pablo Pereira <invict1@outlook.es>
2020-04-18 04:36:12 -0300

    no message

---
+++ Exercises - LeetCode/314.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> verticalOrder(TreeNode* root) {
+        map<int, map<int, vector<int>>> nodes;
+        traverse(root, 0, 0, nodes);
+        vector<vector<int>> resultList;
+        for (auto x : nodes) {
+            vector<int> col;
+            for (auto y : x.second) {
+                col.insert(col.end(), y.second.begin(), y.second.end());
+            }
+            resultList.push_back(col);
+        }
+        return resultList;
+    }
+                                                // x , y, val
+    void traverse(TreeNode* root, int x, int y, map<int, map<int, vector<int>>>& nodes) {
+        if (root) {
+            nodes[x][y].push_back(root -> val);
+            traverse(root -> left, x - 1, y + 1, nodes);
+            traverse(root -> right, x + 1, y + 1, nodes);
+        }
+    }
+};
