19fa38f60b7f27da2b4be581bf047a364d9285a8
Pablo Pereira <invict1@outlook.es>
2020-04-24 19:36:13 -0300

    no message

--- Exercises - LeetCode/31.cpp
+++ Exercises - LeetCode/31.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        bool permutationDone = false;
-        for (int i=nums.size()-1; i > 0 && !permutationDone ; i--) {
-            if (nums[i] > nums[i-1]) {
-                swap(nums[i-1], nums[nums.size()-1]); // CASE: [1,3,2] = [2,1,3]
-                sort(nums.begin() + i, nums.end());
-                reverse(nums.begin() + i, nums.end());
-                permutationDone = true;
+        int i = nums.size() - 2;
+        while (i >= 0 && nums[i+1] <= nums[i]) {
+            i--;
+        }
+        if (i >= 0) {
+            int j = nums.size() - 1;
+            while(j >= 0 && nums[j] <= nums[i]) {
+                j--;
             }
+            swap(nums[i], nums[j]);
         }
-        if (!permutationDone) {
-            reverse(nums.begin(), nums.end());
-        }
+        reverse(nums.begin() + i + 1, nums.end());
     }
 };
