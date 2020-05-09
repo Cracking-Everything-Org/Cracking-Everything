5b81929a98e3debdecc73e94631f6a50eb875338
Pablo Pereira <invict1@outlook.es>
2020-05-09 12:33:01 -0300

    no message

--- Exercises - LeetCode/345.cpp
+++ Exercises - LeetCode/345.cpp
@@ -1,29 +1,25 @@
 class Solution {
 public:
     string reverseVowels(string s) {
-        if(s.length()==0) return s;
-        int first = 0;
-        int second = s.length()-1;
-        unordered_set<char> hs;
-        hs.insert('a');
-        hs.insert('e');
-        hs.insert('i');
-        hs.insert('o');
-        hs.insert('u');
-        hs.insert('A');
-        hs.insert('E');
-        hs.insert('I');
-        hs.insert('O');
-        hs.insert('U');
-        while(first<second){
-            if(hs.find(s[first]) != hs.end() && hs.find(s[second]) != hs.end()){
-                swap(s[first], s[second]);
-                first++;
-                second--;
-            } else {
-                if(hs.find(s[first]) != hs.end()) second--;
-                else first++;
+        unordered_map<char, bool> ht =
+        {{'a',true},
+        {'e',true},
+        {'i',true},
+        {'o',true},
+        {'u',true}};
+
+        int leftPtr = 0;
+        int rightPtr = s.length() - 1;
+        while (leftPtr < rightPtr) {
+            while (ht.find(tolower(s[leftPtr])) == ht.end() && leftPtr < rightPtr) {
+                leftPtr++;
             }
+            while (ht.find(tolower(s[rightPtr])) == ht.end() && leftPtr < rightPtr) {
+                rightPtr--;
+            }
+            swap(s[leftPtr], s[rightPtr]);
+            leftPtr++;
+            rightPtr--;
         }
         return s;
     }
