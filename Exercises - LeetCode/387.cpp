92fe8add8384bb6ec21a0e87a53ba00eba31568f
Pablo Pereira <invict1@outlook.es>
2020-05-13 21:21:25 -0300

    no message

--- Exercises - LeetCode/387.cpp
+++ Exercises - LeetCode/387.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     int firstUniqChar(string s) {
         unordered_map<char,int> ht;
-        for(auto c : s){
-            if(ht.find(c) == ht.end()){
+        for (auto c : s) {
+            if (ht.find(c) == ht.end()) {
                 ht[c] = 1;
-            }else {
+            } else {
                 ht[c]++;
             }
         }
-        for(int i=0; i<s.length(); i++){
-            if(ht[s[i]]==1) return i;
+        for (int i=0; i<s.length(); i++) {
+            if (ht[s[i]]==1) return i;
         }
         return -1;
     }
