10de3a7e3ce8cc3a919ac598f138a1d4751fc879
Pablo Pereira <invict1@outlook.es>
2020-05-15 22:37:44 -0300

    no message

--- Exercises - LeetCode/786.cpp
+++ Exercises - LeetCode/786.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     vector<int> kthSmallestPrimeFraction(vector<int>& A, int K) {
         priority_queue<pair<double, pair<int,int> > > maxHeap;
-        for(int i=0; i<A.size(); i++) {
-            for(int j=i+1; j<A.size(); j++){
-                if(maxHeap.size()<K){
+        for (int i=0; i<A.size(); i++) {
+            for (int j=i+1; j<A.size(); j++) {
+                if (maxHeap.size()<K) {
                     maxHeap.push(make_pair((double)(A[i])/A[j], make_pair(A[i], A[j])));
-                }else{
-                   if((double)A[i]/A[j] < maxHeap.top().first){
+                } else {
+                   if ((double)A[i]/A[j] < maxHeap.top().first) {
                         maxHeap.pop();
                         maxHeap.push(make_pair((double)A[i]/A[j], make_pair(A[i], A[j])));
                     }
