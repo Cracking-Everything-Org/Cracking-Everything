41592caf21e13773f960f1bed2bfe2219088eb63
Pablo Pereira <invict1@outlook.es>
2020-05-18 21:53:04 -0300

    no message

--- Exercises - LeetCode/520.cpp
+++ Exercises - LeetCode/520.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     bool detectCapitalUse(string word) {
-        if(word.length()<2) return true;
-        if(islower(word[0]) && isupper(word[1])) return false;
+        if (word.length()<2) return true;
+        if (islower(word[0]) && isupper(word[1])) return false;
         bool shouldBeCaps = isupper(word[1]);
-        for(int i=2; i<word.length(); i++){
-            if(shouldBeCaps && islower(word[i])) return false;
-            if(!shouldBeCaps && isupper(word[i])) return false;
+        for (int i=2; i<word.length(); i++) {
+            if (shouldBeCaps && islower(word[i])) return false;
+            if (!shouldBeCaps && isupper(word[i])) return false;
         }
         return true;
     }
