a8cbf720f3aacbcb577e9fa9da9349c812cb926e
Pablo Pereira <invict1@outlook.es>
2020-05-19 19:34:27 -0300

    no message

--- Exercises - LeetCode/1160.cpp
+++ Exercises - LeetCode/1160.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     bool goodString(unordered_map<char, int> iterationHT, unordered_map<char, int> ht) {
-        for(auto it: iterationHT) {
-            if(ht.find(it.first)== ht.end()) return false;
-            if(ht[it.first] < it.second) return false;
+        for (auto it: iterationHT) {
+            if (ht.find(it.first)== ht.end()) return false;
+            if (ht[it.first] < it.second) return false;
         }
         return true;
     }
@@ -11,11 +11,11 @@ public:
     int countCharacters(vector<string>& words, string chars) {
         unordered_map<char, int> ht;
         int result = 0;
-        for(auto c: chars) ht[c]++;
-        for(auto s: words){
+        for (auto c: chars) ht[c]++;
+        for (auto s: words) {
             unordered_map<char, int> iterationHT;
-            for(auto ch: s) iterationHT[ch]++;
-            if(goodString(iterationHT, ht)) result += s.length();
+            for (auto ch: s) iterationHT[ch]++;
+            if (goodString(iterationHT, ht)) result += s.length();
         }
         return result;
     }
