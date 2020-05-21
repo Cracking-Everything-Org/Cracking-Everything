e430e7af8f3f42de789ee2e7f5f9b17402795755
Pablo Pereira <invict1@outlook.es>
2020-05-20 23:57:12 -0300

    .

--- Exercises - LeetCode/917.cpp
+++ Exercises - LeetCode/917.cpp
@@ -4,9 +4,9 @@ public:
         string copy = str.copy(S);
         int last=S.length()-1;
         int first= 0;
-        while(first < last){
-            while(!isalpha(S[first]) && first < last) first++;
-            while(!isalpha(S[last]) && first < last) last--;
+        while (first < last) {
+            while (!isalpha(S[first]) && first < last) first++;
+            while (!isalpha(S[last]) && first < last) last--;
             swap(S[first], S[last]);
         }
         return S;
