91720ef27b58aa2cd563476c67cd234c9d3cbf78
Pablo Pereira <invict1@outlook.es>
2020-05-22 23:44:42 -0300

    no message

--- Exercises - LeetCode/572.cpp
+++ Exercises - LeetCode/572.cpp
@@ -10,22 +10,22 @@
 class Solution {
 public:
     bool isSubtree(TreeNode* s, TreeNode* t) {
-        if(!s || !t) return false;
-        if(s->val == t->val){
+        if (!s || !t) return false;
+        if (s->val == t->val) {
             return isSubtreeAux(s, t)
                 || isSubtree(s->right, t)
                 || isSubtree(s->left, t);
         }
-        else{
+        else {
             return isSubtree(s->right, t)
                 || isSubtree(s->left, t);
         }
     }
 
-    bool isSubtreeAux(TreeNode* s, TreeNode* t){
-        if(!s && !t) return true;
-        if(!s || !t) return false;
-        if(s->val != t->val) return false;
+    bool isSubtreeAux(TreeNode* s, TreeNode* t) {
+        if (!s && !t) return true;
+        if (!s || !t) return false;
+        if (s->val != t->val) return false;
         return isSubtreeAux(s->left, t->left)
             && isSubtreeAux(s->right, t->right);
     }
