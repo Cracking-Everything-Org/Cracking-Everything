378cc3031f3f60d31b0720572338b72b5aa30060
Pablo Pereira <invict1@outlook.es>
2020-05-29 23:40:38 -0300

    no message

--- Exercises - LeetCode/383.cpp
+++ Exercises - LeetCode/383.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
         unordered_map<char,int> ht;
-        for(auto c : magazine){
+        for (auto c : magazine) {
             if(ht.find(c)!=ht.end()) ht[c]++;
             else ht[c]=1;
         }
-        for(auto r : ransomNote){
-            if(ht.find(r)!=ht.end()) {
+        for (auto r : ransomNote) {
+            if (ht.find(r)!=ht.end()) {
                 ht[r]--;
-                if(ht[r]==0) ht.erase(r);
+                if (ht[r]==0) ht.erase(r);
             } else return false;
         }
         return true;
