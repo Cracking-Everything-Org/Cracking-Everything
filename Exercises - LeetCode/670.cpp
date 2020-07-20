63dfd7dd7e7742a632810747d33d9d8deaeeab2a
Pablo Pereira <invict1@outlook.es>
2020-07-20 02:12:08 -0300

    no message

--- Exercises - LeetCode/670.cpp
+++ Exercises - LeetCode/670.cpp
@@ -3,11 +3,11 @@ public:
     int maximumSwap(int num) {
         string s = to_string(num);
         vector<int>bucket(10);
-        for(int i = 0; i < s.size(); i++)
+        for (int i = 0; i < s.size(); i++)
             bucket[s[i] - '0'] = i;
-        for(int i = 0; i < s.size(); i++)
-            for(int  j = 9; j > s[i] - '0'; j--)
-                if(bucket[j] > i){
+        for (int i = 0; i < s.size(); i++)
+            for (int  j = 9; j > s[i] - '0'; j--)
+                if (bucket[j] > i) {
                     swap(s[i], s[bucket[j]]);
                     return stoi(s);
                 }
