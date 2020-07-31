4d008fc6ee0959f355cc0f0ff70bd73409837e40
Pablo Pereira <invict1@outlook.es>
2020-07-30 23:50:23 -0300

    no message

--- Exercises - LeetCode/389.cpp
+++ Exercises - LeetCode/389.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     char findTheDifference(string s, string t) {
         int sumS = 0;
-        for(int i = 0; i< s.length(); i++) sumS -= s[i];
-        for(int j = 0; j< t.length(); j++) sumS += t[j];
+        for (int i = 0; i< s.length(); i++) sumS -= s[i];
+        for (int j = 0; j< t.length(); j++) sumS += t[j];
         return (char)sumS;
     }
 };
