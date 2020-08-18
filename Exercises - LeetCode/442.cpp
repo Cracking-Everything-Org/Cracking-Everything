cafed87489ac430b120f742fa105a3a1c9324eed
Pablo Pereira <invict1@outlook.es>
2020-08-18 01:38:17 -0300

    no message

---
+++ Exercises - LeetCode/442.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        vector<int> duplicates;
+        int i = 0;
+        while (i < nums.size()) {
+            if (nums[i] != nums[nums[i] - 1]) {
+                swap(nums[i], nums[nums[i] - 1]);
+            } else {
+                i++;
+            }
+        }
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != i + 1)
+                duplicates.push_back(nums[i]);
+        }
+        return duplicates;
+    }
+};
