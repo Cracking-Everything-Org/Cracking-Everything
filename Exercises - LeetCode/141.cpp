48f7503163ecc9b87a8871681e5c0a34d1059be6
Pablo Pereira <invict1@outlook.es>
2020-08-23 15:44:39 -0300

    no message

--- Exercises - LeetCode/141.cpp
+++ Exercises - LeetCode/141.cpp
@@ -9,14 +9,15 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        if(!head || !head->next) return false;
-        ListNode *slow = head;
-        ListNode *fast = head;
-        while(fast->next && fast->next->next){
-            slow=slow->next;
-            fast=fast->next->next;
-            if(slow==fast)return true;
+        if (!head || !head->next) return false;
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+
+        while (slow != fast) {
+            if (!fast || !fast->next) return false;
+            slow = slow->next;
+            fast = fast->next->next;
         }
-        return false;
+        return true;
     }
 };
