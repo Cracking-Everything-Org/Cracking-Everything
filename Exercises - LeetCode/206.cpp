dea6935796433bf488b174bcca5a6c868bc473d9
Pablo Pereira <invict1@outlook.es>
2020-08-25 11:07:42 -0300

    no message

--- Exercises - LeetCode/206.cpp
+++ Exercises - LeetCode/206.cpp
@@ -5,32 +5,14 @@
  *     ListNode *next;
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
-
- ListNode* reverseList(ListNode* head) {
-        if(!head || !head->next) return head;
-        ListNode* prev = NULL;
-        ListNode* cur = head;
-        ListNode* next = NULL;
-        while(cur){
-            next = cur->next;
-            cur->next = prev;
-            prev = cur;
-            cur = next;
-        }
-        return prev;
-    }
-
-
  */
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        if(!head || !head->next) {
-            return head;
-        }
-        ListNode* start = reverseList(head->next);
+        if (!head || !head->next) return head;
+        ListNode* rest = reverseList(head->next);
         head->next->next = head;
         head->next = NULL;
-        return start;
+        return rest;
     }
 };
