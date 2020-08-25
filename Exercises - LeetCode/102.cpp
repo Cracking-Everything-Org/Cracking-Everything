f10c13ac9336f660575328c2fa59a4b607ac5f60
Pablo Pereira <invict1@outlook.es>
2020-08-25 11:07:14 -0300

    no message

--- Exercises - LeetCode/102.cpp
+++ Exercises - LeetCode/102.cpp
@@ -15,20 +15,16 @@ public:
         queue<TreeNode*> q;
         q.push(root);
         while (!q.empty()) {
-            vector<int> currentLevel;
-            int n = q.size();
-            for (int i=0; i<n; i++) {
+            vector<int> level;
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
                 TreeNode* current = q.front();
                 q.pop();
-                currentLevel.push_back(current->val);
-                if (current->left) {
-                    q.push(current->left);
-                }
-                if (current->right) {
-                    q.push(current->right);
-                }
+                level.push_back(current->val);
+                if (current->left) q.push(current->left);
+                if (current->right) q.push(current->right);
             }
-            levels.push_back(currentLevel);
+            levels.push_back(level);
         }
         return levels;
     }
