8752a0cc1d5e525410049d49d7cd450b469ffa73
Pablo Pereira <invict1@outlook.es>
2020-08-25 16:57:21 -0300

    no message

--- Exercises - LeetCode/543.cpp
+++ Exercises - LeetCode/543.cpp
@@ -10,17 +10,19 @@
 class Solution {
 public:
     int diameterOfBinaryTree(TreeNode* root) {
-        if(!root) return 0;
-        int maxDia = 1;
-        diameter(root, maxDia);
-        return maxDia - 1;
+        if (!root) return 1;
+        int maxDiameter = 1;
+        depth(root, maxDiameter);
+        return maxDiameter - 1;
     }
 
-    int diameter(TreeNode* root, int& maxDia) {
+    int depth(TreeNode* root, int& maxDiameter) {
         if (!root) return 0;
-        int leftDiameter = diameter(root->left, maxDia);
-        int rightDiameter = diameter(root->right, maxDia);
-        maxDia = max(maxDia, leftDiameter + rightDiameter + 1);
-        return max(leftDiameter, rightDiameter) + 1;
+        int leftHeight = depth(root->left, maxDiameter);
+        int rightHeight = depth(root->right, maxDiameter);
+        // global
+        maxDiameter = max(maxDiameter, leftHeight + rightHeight + 1);
+        // local
+        return max(leftHeight, rightHeight) + 1;
     }
 };
