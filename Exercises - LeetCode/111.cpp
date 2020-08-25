76691fe43e3011dd68f1870d3bfac357741d61c7
Pablo Pereira <invict1@outlook.es>
2020-08-25 11:29:41 -0300

    no message

--- Exercises - LeetCode/111.cpp
+++ Exercises - LeetCode/111.cpp
@@ -15,7 +15,6 @@ public:
         queue<TreeNode*> q;
         q.push(root);
         while (!q.empty()) {
-
             int size = q.size();
             for (int i = 0; i < size; i++) {
                 TreeNode* current = q.front();
