5ef469c70af8969da009cb66a43d2a1c9fa0f639
Pablo Pereira <invict1@outlook.es>
2020-08-26 15:53:42 -0300

    no message

--- Exercises - LeetCode/78.cpp
+++ Exercises - LeetCode/78.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> ans;
-        ans.push_back(vector<int>());
-
-        for (auto num : nums) {
-            int n = ans.size();
-            for (int i = 0; i < n; i++) {
-                vector<int> copy(ans[i]);
-                copy.push_back(num);
-                ans.push_back(copy);
+        vector<vector<int>> sets;
+        sets.push_back(vector<int>());
+        for (int i = 0; i < nums.size(); i++) {
+            int setsSize = sets.size();
+            for (int j = 0; j < setsSize; j++) {
+                vector<int> copy(sets[j]);
+                copy.push_back(nums[i]);
+                sets.push_back(copy);
             }
         }
-        return ans;
+        return sets;
     }
 };
