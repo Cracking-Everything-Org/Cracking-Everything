6c486d5f76a41a915accce4dab61edb5bc9ddfba
Pablo Pereira <invict1@outlook.es>
2020-08-28 19:38:20 -0300

    no message

--- Exercises - LeetCode/215.cpp
+++ Exercises - LeetCode/215.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-
         priority_queue<int, vector<int>, greater<int>> minHeap;
         for (int i=0; i<nums.size(); i++) {
             minHeap.push(nums[i]);
@@ -11,4 +10,10 @@ public:
         }
         return minHeap.top();
     }
+
+    /*struct compare {
+        bool operator()(int a, int b){
+            return a > b;
+        }
+    };*/
 };
