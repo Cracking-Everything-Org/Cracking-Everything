ac621f67a2690eeb506b45aae607c1a3d6f27171
Pablo Pereira <invict1@outlook.es>
2020-08-27 21:20:07 -0300

    no message

--- Exercises - LeetCode/268.cpp
+++ Exercises - LeetCode/268.cpp
@@ -1,3 +1,25 @@
+// bit manip - controla el overflow que puede causar al tratar de acceder a una posicion de un input muy grande
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+
+        int x1 = 1;
+        for (int i = 2; i < n + 1; i++) {
+            x1 = x1 ^ i;
+        }
+
+        int x2 = nums[0];
+        for (int i = 1; i < n; i++) {
+            x2 = x2 ^ nums[i];
+        }
+
+        return x1 ^ x2;
+    }
+};
+
+/* cyclic sort
+
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
@@ -16,3 +38,4 @@ public:
         return nums.size();
     }
 };
+*/
