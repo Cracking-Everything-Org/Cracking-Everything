729dbecdc5eb072f032f3654daa43f0cd8c59585
Pablo Pereira <invict1@outlook.es>
2020-09-01 19:41:23 -0300

    no message

---
+++ Exercises - LeetCode/1025.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool divisorGame(int N) {
+        vector<bool> memo(N+1, false);
+        return divisor(N, memo);
+    }
+
+    bool divisor(int N, vector<bool>& memo) {
+        bool result = false;
+        if (memo[N]) {
+            return memo[N];
+        }
+        for (int x = 1; !result && x <= N / 2; x++) {
+            if (N % x == 0) {
+                result = !divisor(N-x, memo);
+            }
+        }
+        memo[N] = result;
+        return memo[N];
+    }
+
+};
