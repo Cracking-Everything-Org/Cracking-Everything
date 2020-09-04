2c2ecff6a33504945dbe92a426a5f8570cc035d6
Pablo Pereira <invict1@outlook.es>
2020-09-04 16:21:37 -0300

    no message

---
+++ Exercises - LeetCode/1428.cpp
@@ -0,0 +1,31 @@
+/**
+ * // This is the BinaryMatrix's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * class BinaryMatrix {
+ *   public:
+ *     int get(int row, int col);
+ *     vector<int> dimensions();
+ * };
+ */
+
+class Solution {
+public:
+    int leftMostColumnWithOne(BinaryMatrix &binaryMatrix) {
+
+        int rows = binaryMatrix.dimensions()[0];
+        int cols = binaryMatrix.dimensions()[1];
+
+        int row = 0;
+        int col = cols - 1;
+
+        while (row < rows && col >= 0) {
+            if (binaryMatrix.get(row, col) == 0) {
+                row++;
+            } else {
+                col--;
+            }
+        }
+
+        return (col == cols - 1) ? -1 : col + 1;
+    }
+};
