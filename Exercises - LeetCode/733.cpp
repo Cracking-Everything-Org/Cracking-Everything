10230f5a7a1dc6626cc3b9ce6f60eb3264dcd5ee
Pablo Pereira <invict1@outlook.es>
2020-09-04 16:21:27 -0300

    no message

--- Exercises - LeetCode/733.cpp
+++ Exercises - LeetCode/733.cpp
@@ -1,24 +1,31 @@
 class Solution {
 public:
-
-    void fill(vector<vector<int>>&  image, int sr, int sc, int color, int newColor){
-        if(   sr < 0
-           || sr >= image.size()
-           || sc < 0
-           || sc >= image[sr].size()
-           || image[sr][sc] != color) return;
-
-        image[sr][sc] = newColor;
-
-        fill(image, sr+1, sc, color, newColor);
-        fill(image, sr-1, sc, color, newColor);
-        fill(image, sr, sc+1, color, newColor);
-        fill(image, sr, sc-1, color, newColor);
-    }
-
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
-        if(image[sr][sc] == newColor) return image;
-        fill(image, sr, sc, image[sr][sc], newColor);
+        if (image.size() <= 0 || newColor == image[sr][sc]) {
+            return image;
+        }
+        int originalColor = image[sr][sc];
+        queue<pair<int,int>> toProcess;
+        toProcess.push(make_pair(sr,sc));
+        while (!toProcess.empty()) {
+            pair<int,int> current = toProcess.front();
+            toProcess.pop();
+            if (image[current.first][current.second] == originalColor) {
+                image[current.first][current.second] = newColor;
+                if (current.first - 1 >= 0) {
+                toProcess.push(make_pair(current.first - 1, current.second));
+                }
+                if (current.first + 1 < image.size()) {
+                    toProcess.push(make_pair(current.first + 1, current.second));
+                }
+                if (current.second - 1 >= 0) {
+                    toProcess.push(make_pair(current.first, current.second - 1));
+                }
+                if (current.second + 1 < image[0].size()) {
+                    toProcess.push(make_pair(current.first, current.second + 1));
+                }
+            }
+        }
         return image;
     }
 };
