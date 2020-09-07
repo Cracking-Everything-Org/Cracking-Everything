b93ed711b5afeb0c5a03905b9c968d886a1d3a4b
Pablo Pereira <invict1@outlook.es>
2020-09-07 14:35:57 -0300

    no message

--- Exercises - LeetCode/1423.cpp
+++ Exercises - LeetCode/1423.cpp
@@ -1,25 +1,29 @@
 class Solution {
 public:
+    // SLIDING WINDOW
     int maxScore(vector<int>& cardPoints, int k) {
-        vector<vector<int>> memo(k + 1, vector<int>(k + 1, -1));
-        return getMaxScore(cardPoints, k, 0, cardPoints.size() - 1, 0, memo);
-    }
-
-    int getMaxScore(vector<int>& cardPoints, int k, int left, int right, int points, vector<vector<int>>& memo) {
-        // ya no tengo como retirar cartas
-        if (k == 0) {
-            return points;
+        if (cardPoints.size() < k) {
+            return -1;
         }
-        // si me voy de rango ya no es valido, "You have to take exactly k cards"
-        if (left > cardPoints.size() - 1 || right < 0) {
-            return INT_MIN;
+        vector<int> candidates;
+        int size = cardPoints.size() - 1;
+        for (int i = size - k + 1; i <= size; i++) {
+            candidates.push_back(cardPoints[i]);
         }
-        // si ya conozco el valor para esa combinacion de left/right
-        if (memo[left][cardPoints.size() - 1 - right] != -1) {
-            return memo[left][cardPoints.size() - 1 - right];
+        for (int i = 0; i < k; i++) {
+            candidates.push_back(cardPoints[i]);
         }
-
-        int ans = max( getMaxScore(cardPoints, k-1, left + 1, right, points + cardPoints[left], memo) , getMaxScore(cardPoints, k-1, left, right - 1, points + cardPoints[right], memo) );
-        return memo[left][cardPoints.size() - 1 - right] = ans;
+        int maxPoints = INT_MIN;
+        int points = 0;
+        int startWindow = 0;
+        for (int endWindow = 0; endWindow < candidates.size(); endWindow++) {
+            points += candidates[endWindow];
+            if (endWindow - startWindow + 1 >= k) {
+                maxPoints = maxPoints < points ? points : maxPoints;
+                points -= candidates[startWindow];
+                startWindow++;
+            }
+        }
+        return maxPoints;
     }
 };
