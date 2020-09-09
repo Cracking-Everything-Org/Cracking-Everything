466bdc5f57a8c6304905b42a22479ffd639961e8
Pablo Pereira <invict1@outlook.es>
2020-09-08 22:30:45 -0300

    no message

---
+++ Exercises - LeetCode/1055.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int shortestWay(string s, string t) {
+        int numsSubsequences = 0;
+        string remaining = t;
+
+        while (remaining.length() > 0) {
+            int sIndex = 0;
+            int rIndex = 0;
+            string subsequence = "";
+
+            while (sIndex < s.length() && rIndex < remaining.length()) {
+                if (s[sIndex++] == remaining[rIndex]) {
+                    subsequence += remaining[rIndex++];
+                }
+            }
+
+            if (subsequence.length() == 0) {
+                return -1;
+            }
+
+            numsSubsequences++;
+            remaining = remaining.substr(subsequence.length());
+
+        }
+
+        return numsSubsequences;
+
+    }
+};
