a905cc239e17ec5c33b447a32bcfa30a2429b4a8
Pablo Pereira <invict1@outlook.es>
2020-09-25 17:05:12 -0300

    no message

--- Exercises - LeetCode/189.cpp
+++ Exercises - LeetCode/189.cpp
@@ -3,8 +3,8 @@ public:
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
         vector<int> numsCopy(nums);
- 
-        for(int i = 0; i < n; i++)
+
+        for (int i = 0; i < n; i++)
             nums[(i+k)%n] = numsCopy[i];
     }
 };
