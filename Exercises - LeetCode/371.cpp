be9b65cb7a54e3938a95cac362fff1314a13b235
Pablo Pereira <invict1@outlook.es>
2020-09-26 20:24:15 -0300

    no message

--- Exercises - LeetCode/371.cpp
+++ Exercises - LeetCode/371.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int getSum(int a, int b) {
-        while(b != 0){
+        while (b != 0) {
             int carry = a & b;
             a = a ^ b;
             b = carry << 1;
