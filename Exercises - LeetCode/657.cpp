b82984ae412ef090480520d820de474771f8d96a
Pablo Pereira <invict1@outlook.es>
2020-10-02 20:33:09 -0300

    no message

--- Exercises - LeetCode/657.cpp
+++ Exercises - LeetCode/657.cpp
@@ -3,8 +3,8 @@ public:
     bool judgeCircle(string moves) {
         int x = 0;
         int y = 0;
-        for(auto c : moves){
-            if(c=='U') y++;
+        for (auto c : moves) {
+            if (c=='U') y++;
             else if (c=='D') y--;
             else if (c=='R') x++;
             else x--;
