5a594a078576e57343c5bf6cf3230da4b3c0720f
Pablo Pereira <invict1@outlook.es>
2020-10-21 13:15:40 -0300

    Update 239.cpp

--- Exercises - LeetCode/239.cpp
+++ Exercises - LeetCode/239.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-
+        
         vector<int> maxList;
-
         int startPointer = 0;
         int currentMax = INT_MIN;
+        
         for (int endPointer = 0; endPointer < nums.size(); endPointer++) {
-
             if (endPointer - startPointer + 1 == k) {
-                for (int i=0; i<k ; i++) {
+                for (int i = 0; i < k; i++) {
                     currentMax = max(currentMax, nums[startPointer + i]);
                 }
                 maxList.push_back(currentMax);
@@ -17,7 +16,6 @@ public:
                 startPointer ++;
             }
         }
-
         return maxList;
     }
 };
