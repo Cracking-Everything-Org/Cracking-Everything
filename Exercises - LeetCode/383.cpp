3ef6de4dc063b6b4826691831994f8885f90d0d2
Pablo Pereira <invict1@outlook.es>
2020-10-25 17:07:27 -0300

    Update 383.cpp

--- Exercises - LeetCode/383.cpp
+++ Exercises - LeetCode/383.cpp
@@ -3,13 +3,13 @@ public:
     bool canConstruct(string ransomNote, string magazine) {
         unordered_map<char,int> ht;
         for (auto c : magazine) {
-            if(ht.find(c)!=ht.end()) ht[c]++;
-            else ht[c]=1;
+            if (ht.find(c) != ht.end()) ht[c]++;
+            else ht[c] = 1;
         }
         for (auto r : ransomNote) {
-            if (ht.find(r)!=ht.end()) {
+            if (ht.find(r) != ht.end()) {
                 ht[r]--;
-                if (ht[r]==0) ht.erase(r);
+                if (ht[r] == 0) ht.erase(r);
             } else return false;
         }
         return true;
