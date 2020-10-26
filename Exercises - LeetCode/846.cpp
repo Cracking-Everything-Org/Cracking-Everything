e9ee57295ff12043af9d04afe909d9accb17529b
Pablo Pereira <invict1@outlook.es>
2020-10-26 14:31:08 -0300

    Update 846.cpp

--- Exercises - LeetCode/846.cpp
+++ Exercises - LeetCode/846.cpp
@@ -2,24 +2,24 @@ class Solution {
 public:
     bool isNStraightHand(vector<int>& hand, int W) {
         priority_queue<int, vector<int>, greater<int>> pq;
-        for(auto &n : hand)
+        for (auto &n : hand)
             pq.push(n);
-        while(pq.size()) {
+        while (pq.size()) {
             int n = pq.top();
             vector<int> temp;
-            for(int i = n; i < n + W; i++) {
-                if(pq.top() == i)
+            for (int i = n; i < n + W; i++) {
+                if (pq.top() == i)
                     pq.pop();
                 else
                     return false;
                 // handle duplicates
-                 while(pq.size() and pq.top() == i) {
+                 while (pq.size() and pq.top() == i) {
                     pq.pop();
                     temp.push_back(i);
                 }
             }
             // insert duplicates back
-            for(auto &t : temp)
+            for (auto &t : temp)
                 pq.push(t);
         }
         return true;
