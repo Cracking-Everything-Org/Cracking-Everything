4f844b2ace2f38acfe67bd013d150d78a73c963c
Pablo Pereira <invict1@outlook.es>
2020-10-27 15:28:23 -0300

    Update 88.cpp

--- Exercises - LeetCode/88.cpp
+++ Exercises - LeetCode/88.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
 
-        int finalIndex = m+n-1;
-        int firstIndex = m-1;
-        int secondIndex = n-1;
+        int finalIndex = m + n - 1;
+        int firstIndex = m - 1;
+        int secondIndex = n - 1;
 
-        while(firstIndex>= 0 && secondIndex>=0){
-            if(nums2[secondIndex] >= nums1[firstIndex]){
+        while (firstIndex >= 0 && secondIndex >= 0) {
+            if (nums2[secondIndex] >= nums1[firstIndex]) {
                 nums1[finalIndex] = nums2[secondIndex];
                 secondIndex--;
             } else {
@@ -16,12 +16,12 @@ public:
             }
             finalIndex--;
         }
-        while(firstIndex>=0){
+        while (firstIndex >= 0) {
             nums1[finalIndex] = nums1[firstIndex];
             firstIndex--;
             finalIndex--;
         }
-        while(secondIndex>=0){
+        while (secondIndex >= 0) {
             nums1[finalIndex] = nums2[secondIndex];
             finalIndex--;
             secondIndex--;
