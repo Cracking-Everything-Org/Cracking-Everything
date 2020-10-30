44325b454cc9ebb98e062b33bd90848d42917577
Pablo Pereira <invict1@outlook.es>
2020-10-29 22:44:13 -0300

    Update 994.cpp

--- Exercises - LeetCode/994.cpp
+++ Exercises - LeetCode/994.cpp
@@ -5,8 +5,8 @@ public:
 
         queue<pair<int, int>> q;
 
-        for (int i=0; i<grid.size(); i++) {
-            for (int j=0; j<grid[0].size(); j++) {
+        for (int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid[0].size(); j++) {
                 if (grid[i][j] == 2) {
                     q.push(make_pair(i,j));
                 }
@@ -19,17 +19,17 @@ public:
                 pair<int,int> c = q.front();
                 q.pop();
                 grid[c.first][c.second] = 2;
-                if (c.first-1 > 0) { q.push(make_pair(c.first-1, c.second)); }
-                if (c.first+1 < grid.size()) { q.push(make_pair(c.first+1, c.second)); }
-                if (c.second-1 > 0) { q.push(make_pair(c.first, c.second-1)); }
-                if (c.second+1 < grid[0].size()) { q.push(make_pair(c.first, c.second+1)); }
+                if (c.first - 1 > 0) { q.push(make_pair(c.first - 1, c.second)); }
+                if (c.first + 1 < grid.size()) { q.push(make_pair(c.first + 1, c.second)); }
+                if (c.second - 1 > 0) { q.push(make_pair(c.first, c.second - 1)); }
+                if (c.second + 1 < grid[0].size()) { q.push(make_pair(c.first, c.second + 1)); }
 
             }
             minutes++;
         }
 
-        for (int i=0; i<grid.size(); i++) {
-            for (int j=0; j<grid[0].size(); j++) {
+        for (int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid[0].size(); j++) {
                 if (grid[i][j] == 1) {
                     return -1;
                 }
