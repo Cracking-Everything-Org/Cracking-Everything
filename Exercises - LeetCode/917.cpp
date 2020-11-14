463fbfd80a7f97745ca9e0953c8b4969dc23d921
Pablo Pereira <invict1@outlook.es>
2020-11-14 12:25:44 -0300

    Update 917.cpp

--- Exercises - LeetCode/917.cpp
+++ Exercises - LeetCode/917.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     string reverseOnlyLetters(string S) {
         string copy = str.copy(S);
-        int last=S.length()-1;
-        int first= 0;
+        int last = S.length() - 1;
+        int first = 0;
         while (first < last) {
             while (!isalpha(S[first]) && first < last) first++;
             while (!isalpha(S[last]) && first < last) last--;
