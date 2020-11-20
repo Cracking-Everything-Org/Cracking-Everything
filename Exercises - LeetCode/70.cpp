8f0662219b9e5b8f89ab17089df478cdbb376295
Pablo Pereira <invict1@outlook.es>
2020-11-20 10:30:39 -0300

    Update 70.cpp

--- Exercises - LeetCode/70.cpp
+++ Exercises - LeetCode/70.cpp
@@ -6,9 +6,9 @@ public:
     }
 
     int climb(int pos, int n, unordered_map<int,int>& dp) {
-        if(dp[pos] > 0) return dp[pos];
-        if(pos > n) return 0;
-        if(pos == n) return 1;
+        if (dp[pos] > 0) return dp[pos];
+        if (pos > n) return 0;
+        if (pos == n) return 1;
         dp[pos] = climb(pos + 1, n, dp) + climb(pos + 2, n , dp);
         return dp[pos];
     }
