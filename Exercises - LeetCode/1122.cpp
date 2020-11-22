a057472df7d7f7a73eca60776ee0877cf9bf4276
Pablo Pereira <invict1@outlook.es>
2020-11-22 12:21:57 -0300

    Update 1122.cpp

--- Exercises - LeetCode/1122.cpp
+++ Exercises - LeetCode/1122.cpp
@@ -2,23 +2,23 @@ class Solution {
 public:
     vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
         unordered_map<int, int> m;
-        for(auto it : arr1)
+        for (auto it : arr1)
             m[it]++;
         vector<int> res;
-        for(auto it : arr2) {
+        for (auto it : arr2) {
             while(m[it]) {
                 res.push_back(it);
                 m[it]--;
             }
         }
         vector<int> temp;
-        for(auto it : m) {
+        for (auto it : m) {
             int x = it.second;
-            while(x)
+            while (x)
                 temp.push_back(it.first), x--;
         }
         sort(temp.begin(), temp.end());
-        for(auto it : temp)
+        for (auto it : temp)
             res.push_back(it);
         return res;
     }
