0449a30c62b40df52956a70b390e772e6ff19a5b
Pablo Pereira <invict1@outlook.es>
2020-11-23 09:30:55 -0300

    Update 203.cpp

--- Exercises - LeetCode/203.cpp
+++ Exercises - LeetCode/203.cpp
@@ -9,10 +9,10 @@
 class Solution {
 public:
     ListNode* removeElements(ListNode* head, int val) {
-        while(head && head->val == val) head=head->next;
+        while (head && head->val == val) head = head->next;
         ListNode* start = head;
-        while(head && head->next){
-            if(head->next->val == val){
+        while (head && head->next) {
+            if (head->next->val == val) {
                 head->next = head->next->next;
             }
             else head = head->next;
