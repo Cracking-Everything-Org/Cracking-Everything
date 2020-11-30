a805a994b2d4528ca2d1b5df0c43210344df4e19
Pablo Pereira <invict1@outlook.es>
2020-11-30 20:53:39 -0300

    Update 69.cpp

--- Exercises - LeetCode/69.cpp
+++ Exercises - LeetCode/69.cpp
@@ -4,12 +4,12 @@ public:
         int low = 0;
         int high = x;
         int mid;
-        if(x<2) return x;
-        while(low<high) {
-            mid = (low + high)/2;
-            if(x/mid >= mid) low = mid+1;
+        if (x < 2) return x;
+        while (low < high) {
+            mid = (low + high) / 2;
+            if (x / mid >= mid) low = mid + 1;
             else high = mid;
         }
-        return high-1;
+        return high - 1;
     }
 };
