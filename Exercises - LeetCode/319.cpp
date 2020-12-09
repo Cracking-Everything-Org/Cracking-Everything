6f2c6e492282ab597f9f0057c4a9c19323869264
Pablo Pereira <invict1@outlook.es>
2020-12-09 15:07:59 -0300

    Update 319.cpp

--- Exercises - LeetCode/319.cpp
+++ Exercises - LeetCode/319.cpp
@@ -2,11 +2,9 @@ class Solution {
 public:
     int bulbSwitch(int n) {
         int counts = 0;
-
-        for (int i=1; i*i<=n; ++i) {
-            ++ counts;
+        for (int i = 1; i * i <= n; ++i) {
+            ++counts;
         }
-
         return counts;
     }
     /*Explanation:
