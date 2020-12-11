9c129bcf518ab266997a12d6473f8722594f6e2d
Pablo Pereira <invict1@outlook.es>
2020-12-11 18:19:08 -0300

    Update 238.cpp

--- Exercises - LeetCode/238.cpp
+++ Exercises - LeetCode/238.cpp
@@ -4,14 +4,14 @@ public:
         vector<int> answer(nums.size());
 
         answer[0] = 1;
-        for (int i=1; i<nums.size(); i++) {
+        for (int i = 1; i < nums.size(); i++) {
             answer[i] = nums[i-1] * answer[i-1];
         }
 
         int R = 1;
-        for (int j=nums.size()-1; j>=0; j--) {
+        for (int j = nums.size() - 1; j >= 0; j--) {
             answer[j] = answer[j] * R;
-            R*= nums[j];
+            R *= nums[j];
         }
         return answer;
     }
