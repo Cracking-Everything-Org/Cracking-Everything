caa8b32785ef66349d4d09e179190b93e51b3d57
Pablo Pereira <invict1@outlook.es>
2020-12-10 21:10:22 -0300

    Update 367.cpp

--- Exercises - LeetCode/367.cpp
+++ Exercises - LeetCode/367.cpp
@@ -5,12 +5,12 @@ public:
         int high = num;
         int mid;
         while (low < high) {
-            mid = low+(high-low)/2;
+            mid = low + (high-low) / 2;
             if (pow(mid,2) == num) return true;
             else if (pow(mid,2) > num) {
                 high = mid;
             } else {
-                low = mid+1;
+                low = mid + 1;
             }
         }
         return pow(low,2) == num ? true : false;
