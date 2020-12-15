1facf99af79517060e818806e80d369ee3ce9764
Pablo Pereira <invict1@outlook.es>
2020-12-15 16:12:16 -0300

    Update 922.cpp

--- Exercises - LeetCode/922.cpp
+++ Exercises - LeetCode/922.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     vector<int> sortArrayByParityII(vector<int>& A) {
         int current = 0;
-        int last = A.size()-1;
-        while(current < A.size()) {
-            if(current %2 != 0){
-                while(A[last] %2 == 0) last--;
-                swap(A[current],A[last]);
+        int last = A.size() - 1;
+        while (current < A.size()) {
+            if (current % 2 != 0) {
+                while (A[last] % 2 == 0) last--;
+                swap(A[current], A[last]);
             } else {
-                while(A[last] %2 != 0) last--;
-                swap(A[current],A[last]);
+                while (A[last] % 2 != 0) last--;
+                swap(A[current], A[last]);
             }
-            last = A.size()-1;
+            last = A.size() - 1;
             current++;
         }
         return A;
