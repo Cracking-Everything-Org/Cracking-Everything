13f88eee035546467e115cabce2f199ac18771df
Pablo Pereira <invict1@outlook.es>
2020-12-18 15:03:00 -0300

    Update 146.cpp

--- Exercises - LeetCode/146.cpp
+++ Exercises - LeetCode/146.cpp
@@ -8,7 +8,7 @@ struct DLinkedNode {
 class LRUCache {
 private:
     //Variables
-    unordered_map<int,DLinkedNode*> m; //Key, Value
+    unordered_map<int, DLinkedNode*> m; //Key, Value
     int count;
     int capacity;
     DLinkedNode *head, *tail;
@@ -16,7 +16,7 @@ private:
     //Functions
 
     /* Always add the new node right after head */
-    void addNode(DLinkedNode* node){
+    void addNode(DLinkedNode* node) {
         //Fit inside first
         node->prev = head;
         node->next = head->next;
@@ -26,23 +26,22 @@ private:
     }
 
     /** Remove an existing node from the linked list */
-    void removeNode(DLinkedNode* node){
+    void removeNode(DLinkedNode* node) {
         DLinkedNode *prevNode = node->prev;
         DLinkedNode *nextNode = node->next;
-
         prevNode->next = nextNode;
         nextNode->prev = prevNode;
     }
 
     /** Move certain node in between to the head. */
-    void moveToHead(DLinkedNode* node){
+    void moveToHead(DLinkedNode* node) {
         removeNode(node);
         addNode(node);
     }
 
 public:
     LRUCache(int _capacity) {
-        count=0;
+        count = 0;
         capacity = _capacity;
 
         head = new DLinkedNode();
@@ -56,25 +55,21 @@ public:
     }
 
     int get(int key) {
-
         //If Not in map
-        if(m.find(key)==m.end())
+        if (m.find(key) == m.end())
             return -1;
 
         DLinkedNode *node = m[key];
 
         // Move the accessed node to the head
         moveToHead(node);
-
         return node->value;
-
     }
 
     void put(int key, int value) {
 
         //If found in map
-        if(m.find(key)!=m.end())
-        {
+        if (m.find(key) != m.end()) {
             //Update the value and move it ahead
             m[key]->value = value;
             moveToHead(m[key]);
@@ -88,17 +83,13 @@ public:
 
         m[key] = newNode;
         addNode(newNode);
-
         ++count;
 
         //Remove the element before tail
-        if(count > capacity){
+        if (count > capacity) {
             DLinkedNode *lastNode = tail->prev;
-
             m.erase(lastNode->key);
             removeNode(lastNode);
         }
-
-
     }
 };
