eabde6d97274b55b61e92dddb93caacb8477ef1a
Pablo Pereira <invict1@outlook.es>
2020-12-23 22:30:51 -0300

    Update 234.cpp

--- Exercises - LeetCode/234.cpp
+++ Exercises - LeetCode/234.cpp
@@ -11,20 +11,20 @@
 /*class Solution {
 public:
     bool isPalindrome(ListNode* head) {
-        if(!head) return true;
+        if (!head) return true;
         ListNode* slow = head;
         ListNode* fast = head;
         stack<int> stk;
         while (fast && fast->next) {
             stk.push(slow->val);
-            slow= slow->next;
-            fast= fast->next->next;
+            slow = slow->next;
+            fast = fast->next->next;
         }
         if (fast) slow = slow->next;
         while(slow){
             int toCompare = stk.top();
             stk.pop();
-            if(slow->val != toCompare) return false;
+            if (slow->val != toCompare) return false;
             slow = slow->next;
         }
         return true;
