70e82c8d41a2033b11f2524cdf87ff0496d82cc8
Pablo Pereira <invict1@outlook.es>
2020-12-24 21:52:33 -0300

    Update 1.cpp

--- Exercises - LeetCode/1.cpp
+++ Exercises - LeetCode/1.cpp
@@ -3,11 +3,11 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int,int> ht;
         vector<int> result;
-        for(int i=0; i< nums.size(); i++){
-            if(ht.find(target-nums[i]) != ht.end()) {
+        for (int i = 0; i < nums.size(); i++) {
+            if (ht.find(target - nums[i]) != ht.end()) {
                 result.push_back(i);
-                result.push_back(ht.find(target-nums[i])->second);
-            }else ht[nums[i]]=i;
+                result.push_back(ht.find(target - nums[i])->second);
+            } else ht[nums[i]] = i;
         }
         return result;
     }
@@ -19,8 +19,8 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<int> two = findTwo(nums, target);
-        vector<int> ans(2,-1);
-        for (int i=0; i<nums.size(); i++) {
+        vector<int> ans(2, -1);
+        for (int i = 0; i < nums.size(); i++) {
             if (nums[i] == two[0] || nums[i] == two[1]) {
                 if (ans[0] == -1) ans[0] = i;
                 else ans[1] = i;
@@ -32,7 +32,7 @@ public:
     vector<int> findTwo(vector<int> nums, int target) {
         sort(nums.begin(), nums.end());
         int leftPointer = 0;
-        int rightPointer = nums.size()-1;
+        int rightPointer = nums.size() - 1;
         while (leftPointer < rightPointer) {
             int sum = nums[leftPointer] + nums[rightPointer];
             if (sum == target) {
@@ -45,6 +45,6 @@ public:
                 rightPointer--;
             }
         }
-        return vector<int>{-1,-1};
+        return vector<int>{-1, -1};
     }
 };
