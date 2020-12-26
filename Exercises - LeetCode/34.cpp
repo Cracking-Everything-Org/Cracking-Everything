79e80401074f5f50ebe8652437d21b6f8c2b189b
Pablo Pereira <invict1@outlook.es>
2020-12-25 22:13:26 -0300

    Update 34.cpp

--- Exercises - LeetCode/34.cpp
+++ Exercises - LeetCode/34.cpp
@@ -13,14 +13,14 @@ public:
         int left = 0;
         int right = nums.size()-1;
 
-        while(left <= right) {
-            int middle = left + (right-left) / 2;
-            if(nums[middle] >= target) {
+        while (left <= right) {
+            int middle = left + (right - left) / 2;
+            if (nums[middle] >= target) {
                 right = middle - 1;
             } else {
                 left = middle + 1;
             }
-            if(nums[middle] == target) {
+            if (nums[middle] == target) {
                 index = middle;
             }
         }
@@ -32,14 +32,14 @@ public:
         int left = 0;
         int right = nums.size()-1;
 
-        while(left <= right) {
-            int middle = left + (right-left) / 2;
-            if(nums[middle] <= target) {
+        while (left <= right) {
+            int middle = left + (right - left) / 2;
+            if (nums[middle] <= target) {
                 left = middle + 1;
             } else {
                 right = middle - 1;
             }
-            if(nums[middle] == target) {
+            if (nums[middle] == target) {
                 index = middle;
             }
         }
