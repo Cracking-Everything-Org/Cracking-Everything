dd39191e1ad44b7305fcf2d5006ae5ed1dd2734f
Pablo Pereira <invict1@outlook.es>
2020-12-31 15:40:35 -0300

    Update 101.cpp

--- Exercises - LeetCode/101.cpp
+++ Exercises - LeetCode/101.cpp
@@ -10,15 +10,15 @@
 class Solution {
 public:
     bool isSymmetric(TreeNode* root) {
-        if(!root) return true;
+        if (!root) return true;
         return isSym(root->left, root->right);
     }
 
     bool isSym(TreeNode* r1, TreeNode* r2) {
-        if(!r1 && !r2) return true;
-        if(!r1 || !r2) return false;
-        if(r1->val != r2->val) return false;
-        return isSym(r1->left, r2->right)
+        if (!r1 && !r2) return true;
+        if (!r1 || !r2) return false;
+        if (r1->val != r2->val) return false;
+        return isSym(r1->left, r2->right) 
             && isSym(r1->right, r2->left);
     }
 };
