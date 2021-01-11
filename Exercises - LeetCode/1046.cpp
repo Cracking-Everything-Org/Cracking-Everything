62b13f7d5c08c8a89449365d6173aa551edd9c9c
Pablo Pereira <invict1@outlook.es>
2021-01-11 11:28:45 -0300

    Update 1046.cpp

--- Exercises - LeetCode/1046.cpp
+++ Exercises - LeetCode/1046.cpp
@@ -2,18 +2,18 @@ class Solution {
 public:
     int lastStoneWeight(vector<int>& stones) {
         priority_queue<int> pq;
-        if(stones.size()==0) return 0;
-        for(int i=0; i< stones.size(); i++){
+        if (stones.size() == 0) return 0;
+        for (int i = 0; i < stones.size(); i++) {
             pq.push(stones[i]);
         }
-        while(pq.size()>1){
+        while (pq.size() > 1) {
             int first = pq.top();
             pq.pop();
             int second = pq.top();
             pq.pop();
-            if(first != second) pq.push(first-second);
+            if (first != second) pq.push(first - second);
         }
-        if(pq.empty()) return 0;
+        if (pq.empty()) return 0;
         return pq.top();
     }
 };
