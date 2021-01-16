ebb2df11e6c48ad53636f69390d0b8b01f20963d
Pablo Pereira <invict1@outlook.es>
2021-01-15 23:05:08 -0300

    Update 1047.cpp

--- Exercises - LeetCode/1047.cpp
+++ Exercises - LeetCode/1047.cpp
@@ -3,14 +3,14 @@ public:
     string removeDuplicates(string S) {
         stack<char> stk;
         string result;
-        for(int i=0; i<S.length(); i++) {
-            if(!stk.empty()){
-                if(stk.top() == S[i]){
+        for (int i = 0; i < S.length(); i++) {
+            if (!stk.empty()) {
+                if (stk.top() == S[i]) {
                     stk.pop();
                 } else stk.push(S[i]);
             } else stk.push(S[i]);
         }
-        while(!stk.empty()) {
+        while (!stk.empty()) {
             result.push_back(stk.top());
             stk.pop();
         }
