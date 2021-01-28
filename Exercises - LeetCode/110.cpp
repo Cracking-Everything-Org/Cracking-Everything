3782565867fcb81f5d2a7dc9635ef6ea9ed8ae04
Pablo Pereira <invict1@outlook.es>
2021-01-27 22:08:33 -0300

    Update 110.cpp

--- Exercises - LeetCode/110.cpp
+++ Exercises - LeetCode/110.cpp
@@ -7,19 +7,20 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+
 class Solution {
 public:
     bool isBalanced(TreeNode* root) {
-        if(!root) return true;
+        if (!root) return true;
         int leftH = height(root->left);
         int rightH = height(root->right);
-        return abs(leftH-rightH)<2
+        return abs(leftH - rightH) < 2
             && isBalanced(root->left)
             && isBalanced(root->right);
     }
 
-    int height(TreeNode* root){
-        if(!root) return 0;
+    int height(TreeNode* root) {
+        if (!root) return 0;
         return 1 + max(height(root->left), height(root->right));
     }
 };
