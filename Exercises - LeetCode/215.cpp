d629933aae70588145be93379ef5cf0fa1e3d6cd
Pablo Pereira <invict1@outlook.es>
2021-04-12 21:53:22 -0300

    .

--- Exercises - LeetCode/215.cpp
+++ Exercises - LeetCode/215.cpp
@@ -17,3 +17,18 @@ public:
         }
     };*/
 };
+
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int, vector<int>, greater<int>> minHeap;
+        for (int i = 0; i < nums.size(); i++) {
+            minHeap.push(nums[i]);
+            if (minHeap.size() > k) {
+                minHeap.pop();
+            }
+        }
+        return minHeap.top();
+    }
+};
