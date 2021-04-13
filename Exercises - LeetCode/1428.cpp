ab74e6f3d4e82dc0cc25d44ca47f9f704c2830b0
Pablo Pereira <invict1@outlook.es>
2021-04-13 16:55:08 -0300

    .

--- Exercises - LeetCode/1428.cpp
+++ Exercises - LeetCode/1428.cpp
@@ -29,3 +29,24 @@ public:
         return (col == cols - 1) ? -1 : col + 1;
     }
 };
+
+class Solution {
+public:
+    int leftMostColumnWithOne(BinaryMatrix &binaryMatrix) {
+        vector<int> dimensions = binaryMatrix.dimensions();
+        int rows = dimensions[0];
+        int cols = dimensions[1];
+        
+        int row = 0;
+        int col = cols - 1;
+        
+        while (row < rows && col >= 0) {
+            if (binaryMatrix.get(row, col) == 0) {
+                row++;
+            } else {
+                col--;
+            }
+        }
+        return col == cols - 1 ? -1 : col + 1;
+    }
+};
