86181c07223dedbd0468059383770cdaa9b51ff2
Pablo Pereira <invict1@outlook.es>
2021-04-14 11:41:26 -0300

    .

--- Exercises - LeetCode/938.cpp
+++ Exercises - LeetCode/938.cpp
@@ -15,3 +15,23 @@ public:
         calcRangeSum(root->right, sum, L, R);
     }
 };
+
+class Solution {
+public:
+    int rangeSumBST(TreeNode* root, int low, int high) {
+        if (!root) return 0;
+        int count = 0;
+        findSum(root, low, high, count);
+        return count;
+    }
+    
+    void findSum(TreeNode* root, int low, int high, int& count) {
+        if (root) {
+            if (root->val >= low && root->val <= high) {
+                count += root->val;
+            }
+            findSum(root->left, low, high, count);
+            findSum(root->right, low, high, count);
+        }
+    }
+};
