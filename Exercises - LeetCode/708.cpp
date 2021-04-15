be672009cc0bf0f84efff0a64c9a6a9017eb8275
Pablo Pereira <invict1@outlook.es>
2021-04-15 18:40:52 -0300

    .

--- Exercises - LeetCode/708.cpp
+++ Exercises - LeetCode/708.cpp
@@ -50,3 +50,37 @@ public:
         return dummy;
     }
 };
+
+class Solution {
+public:
+    Node* insert(Node* head, int insertVal) {
+        if (!head) {
+            Node* head = new Node(insertVal);
+            head-> next = head;
+            return head;
+        }
+        
+        Node* start = head;
+        Node* curr = head;
+        
+        while (curr->next != start) {
+            int currentVal = curr->val;
+            int nextVal = curr->next->val;
+            
+            if (currentVal > nextVal && (insertVal >= currentVal || insertVal <= nextVal)) {
+                break;
+            }
+            
+            if (currentVal <= insertVal && insertVal <= nextVal) {
+                break;
+            }
+            
+            curr = curr->next;
+        }
+        
+        Node* toInsert = new Node(insertVal, curr->next);
+        curr->next = toInsert;
+        
+        return start;
+    }
+};
