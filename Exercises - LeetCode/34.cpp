f3f9fe789ab01e5f03cec57363b099bc661689bf
Pablo Pereira <invict1@outlook.es>
2021-04-14 21:13:42 -0300

    .

--- Exercises - LeetCode/34.cpp
+++ Exercises - LeetCode/34.cpp
@@ -47,3 +47,38 @@ public:
     }
 
 };
+
+
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        vector<int> result(2, -1);
+        result[0] = search(nums, target, false);
+        if (result[0] != -1) {
+            result[1] = search(nums, target, true);
+        }
+        return result;
+    }
+    
+    int search(vector<int>& nums, int target, bool findMaxIndex) {
+        int targetIndex = -1;
+        int start = 0;
+        int end = nums.size() - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] > target) {
+                end = mid - 1;
+            } else if (nums[mid] < target) {
+                start = mid + 1;
+            } else {
+                targetIndex = mid;
+                if (findMaxIndex) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
+        }
+        return targetIndex;
+    }
+};
