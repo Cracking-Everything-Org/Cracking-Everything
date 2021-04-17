30b41f12629546ae8f929c66ecd1303f746e1827
Pablo Pereira <invict1@outlook.es>
2021-04-17 00:16:53 -0300

    .

--- Exercises - LeetCode/65.cpp
+++ Exercises - LeetCode/65.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
     bool isNumber(string s) {
-        //trim
         int firstNotSpace = 0;
-        int lastNotSpace = s.length()-1;
+        int lastNotSpace = s.length() - 1;
         while (s[firstNotSpace] == ' ' && firstNotSpace < lastNotSpace) firstNotSpace++;
         while (s[lastNotSpace] == ' ' && lastNotSpace > firstNotSpace) lastNotSpace--;
 
@@ -11,7 +10,7 @@ public:
         bool numSeen = false;
         bool dotSeen = false;
 
-        if(!isdigit(s[lastNotSpace])) return false;
+        if (!isdigit(s[lastNotSpace])) return false;
 
         for (int i = firstNotSpace; i <= lastNotSpace; i++) {
             if (isdigit(s[i])) {
@@ -21,10 +20,10 @@ public:
                 eSeen = true;
                 numSeen = false;
             } else if (s[i] == '.') {
-                if(eSeen || dotSeen) return false;
+                if (eSeen || dotSeen) return false;
                 dotSeen = true;
             } else if (s[i] == '-' || s[i] == '+') {
-                if(i != firstNotSpace || s[i-1] != 'e') return false;
+                if (i != firstNotSpace || s[i - 1] != 'e') return false;
             } else return false;
         }
         return numSeen;
