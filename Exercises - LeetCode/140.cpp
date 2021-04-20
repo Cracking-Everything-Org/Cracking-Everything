87bf2ebfadfd2cc01565d9199c80ed14c7d98188
Pablo Pereira <invict1@outlook.es>
2021-04-19 21:25:00 -0300

    .

--- Exercises - LeetCode/140.cpp
+++ Exercises - LeetCode/140.cpp
@@ -1,22 +1,29 @@
 class Solution {
 public:
     vector<string> wordBreak(string s, vector<string>& wordDict) {
-        int N = s.size();
-        unordered_set<string> wordSet(wordDict.begin(), wordDict.end());
-
-        vector<vector<string>> dp(N + 1, vector<string>());
-        dp[0].emplace_back("");
-        for(int i = 1; i <= N; ++i){
-            for(int j = 0; j < i; ++j){
-                string substr = s.substr(j, i - j);
-                if(dp[j].size() > 0 && wordSet.find(substr) != wordSet.end()){
-                    for(string& l : dp[j]){
-                        dp[i].emplace_back(l + (l == "" ? "" : " ") + substr);
-                    }
+        unordered_map<string, vector<string>> cache;
+        unordered_set<string> dict;
+        for (auto entry : wordDict) dict.insert(entry);
+        return dfs(s, dict, cache);
+    }
+    
+    vector<string> dfs(string s, unordered_set<string>& dict, unordered_map<string, vector<string>>& cache) {
+        if (cache.count(s)) return cache[s];
+        vector<string> result;
+        for (int i = 0; i < s.size(); i++) {
+            string temp = s.substr(0, i + 1);
+            if (dict.find(temp) != dict.end()) {
+                if (i == s.size() - 1) {
+                    result.push_back(temp);
+                    break;
+                }
+                vector<string> res = dfs(s.substr(i + 1), dict, cache);
+                for (int j = 0; j < res.size(); j++) {
+                    result.push_back(temp + " " + res[j]);
                 }
             }
         }
-
-        return dp[N];
+        cache[s] = result;
+        return result;
     }
 };
