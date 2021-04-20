b302c08791a6a844ffddd1d266ff3667919dc27e
Pablo Pereira <invict1@outlook.es>
2021-04-19 21:00:24 -0300

    .

--- Exercises - LeetCode/139.cpp
+++ Exercises - LeetCode/139.cpp
@@ -20,3 +20,26 @@ public:
         return isWordBreak[s.length()];
     }
 };
+
+
+class Solution {
+public:
+	bool wordBreak(string s, vector<string>& wordDict) {
+        int n = s.size();
+		vector<bool> dp(n + 1);
+		dp[0] = true; // empty siempre está incluido
+        unordered_set<string> dict; // hago un dict para saber rapidamente si ya encontré el substring
+        for (auto entry : wordDict) dict.insert(entry);
+
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && dict.find(s.substr(j, i)) != dict.end()) {
+                    dp[i] = true; // marco que encontré hasta ese indice
+                    break;
+                }
+            }
+        }
+        
+        return dp[n];
+	}
+};
