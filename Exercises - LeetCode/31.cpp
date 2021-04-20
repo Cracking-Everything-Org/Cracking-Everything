aaaeaf436be973868fac9e5afcc7f93db15e2e50
Pablo Pereira <invict1@outlook.es>
2021-04-19 23:47:15 -0300

    .

--- Exercises - LeetCode/31.cpp
+++ Exercises - LeetCode/31.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
         int i = nums.size() - 2;
-        while (i >= 0 && nums[i + 1] <= nums[i]) {
+        while (i >= 0 && nums[i + 1] <= nums[i]) { // me muevo mientras el izquierdo sea menor
             i--;
         }
         if (i >= 0) {
             int j = nums.size() - 1;
-            while (j >= 0 && nums[j] <= nums[i]) {
+            while (j >= 0 && nums[j] <= nums[i]) { // me muevo mientras el End sea menor que el que encontré antes
                 j--;
             }
-            swap(nums[i], nums[j]);
+            swap(nums[i], nums[j]); // swapeo
         }
-        reverse(nums.begin() + i + 1, nums.end());
+        reverse(nums.begin() + i + 1, nums.end()); // invierto lo que esta entre
     }
 };
