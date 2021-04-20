ba629755e645079190d824e65c9a1e3aa1db5961
Pablo Pereira <invict1@outlook.es>
2021-04-19 23:41:33 -0300

    .

--- Exercises - LeetCode/31.cpp
+++ Exercises - LeetCode/31.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
         int i = nums.size() - 2;
-        while (i >= 0 && nums[i+1] <= nums[i]) {
+        while (i >= 0 && nums[i + 1] <= nums[i]) {
             i--;
         }
         if (i >= 0) {
