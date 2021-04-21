79242c8bd05f3653bdd968d7791519b19b0b4163
Pablo Pereira <invict1@outlook.es>
2021-04-21 11:42:28 -0300

    .

--- Exercises - LeetCode/56.cpp
+++ Exercises - LeetCode/56.cpp
@@ -2,25 +2,25 @@ class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         if (intervals.size() < 2) return intervals;
-        vector<vector<int>> ans;
+        vector<vector<int>> mergedIntervals;
         sort(intervals.begin(), intervals.end());
-
+        
         int itr = 0;
         int start = intervals[itr][0];
         int end = intervals[itr][1];
-
-        while (itr < intervals.size()-1) {
+        
+        while (itr < intervals.size() - 1) {
             itr++;
             if (intervals[itr][0] <= end) {
                 end = max(end, intervals[itr][1]);
             } else {
-                ans.push_back({start,end});
+                mergedIntervals.push_back({ start, end });
                 start = intervals[itr][0];
                 end = intervals[itr][1];
             }
         }
-        ans.push_back({start,end});
-
-        return ans;
+        mergedIntervals.push_back({start,end});
+        
+        return mergedIntervals;
     }
 };
