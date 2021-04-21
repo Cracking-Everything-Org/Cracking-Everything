c5e56d2324d25ee8b200c7b322cc0f473b61cd42
Pablo Pereira <invict1@outlook.es>
2021-04-21 12:17:51 -0300

    .

--- Exercises - LeetCode/986.cpp
+++ Exercises - LeetCode/986.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
-    vector<vector<int>> intervalIntersection(vector<vector<int>>& A, vector<vector<int>>& B) {
-        vector<vector<int>> ans;
-
+    vector<vector<int>> intervalIntersection(vector<vector<int>>& firstList, vector<vector<int>>& secondList) {
+        vector<vector<int>> intersections;
         int i = 0;
         int j = 0;
-        while (i < A.size() && j < B.size()) {
-
-            if (A[i][0] >= B[j][0] && A[i][0] <= B[j][1] ||
-               B[j][0] >= A[i][0] && B[j][0] <= A[i][1] ) {
-                vector<int> newInterval(2,0);
-                newInterval[0] = max(A[i][0], B[j][0]);
-                newInterval[1] = min(A[i][1], B[j][1]);
-                ans.push_back(newInterval);
+        
+        while (i < firstList.size() && j < secondList.size()) {
+            if ((firstList[i][0] >= secondList[j][0] && firstList[i][0] <= secondList[j][1]) || 
+                (secondList[j][0] >= firstList[i][0] && secondList[j][0] <= firstList[i][1])) {
+                vector<int> intersection(2, 0);
+                intersection[0] = max(firstList[i][0], secondList[j][0]);
+                intersection[1] = min(firstList[i][1], secondList[j][1]);
+                intersections.push_back(intersection);
             }
-
-            if (A[i][1] < B[j][1]) i++;
-            else j++;
+            
+            if (firstList[i][1] < secondList[j][1]) {
+                i++;
+            } else {
+                j++;
+            }
         }
-
-        return ans;
+        
+        return intersections;
     }
 };
