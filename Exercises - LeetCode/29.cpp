07881932dcb34920f6bb94fc556edf2ba8eb7b51
Pablo Pereira <invict1@outlook.es>
2021-04-21 16:30:37 -0300

    .

--- Exercises - LeetCode/29.cpp
+++ Exercises - LeetCode/29.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     int divide(int dividend, int divisor) {
         // BIT MANIPULATION
-
+        
         //OVERFLOW CASE
         if (dividend == INT_MIN && divisor == -1) return INT_MAX;
-
-        int a = abs(dividend);
-        int b = abs(divisor);
+        
+        long a = labs(dividend); // labs es el abs de long
+        long b = labs(divisor);
         int res = 0;
-        while (a >= b) {
+        while (a - b >= 0) {
             int x = 0;
             while(a - (b << x << 1) >= 0){
                 x++;
@@ -17,6 +17,6 @@ public:
             res += 1 << x;
             a -= b << x;
         }
-        return (dividend > 0) == (divisor > 0) ? res : -res;
+        return (dividend >= 0) == (divisor >= 0) ? res : -res;
     }
 };
