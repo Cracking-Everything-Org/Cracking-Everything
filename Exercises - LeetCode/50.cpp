d3cc586b0c9be045eabb648b627c73e5eaca3599
Pablo Pereira <invict1@outlook.es>
2021-04-22 19:44:28 -0300

    .

--- Exercises - LeetCode/50.cpp
+++ Exercises - LeetCode/50.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-
-    /*  FAST EXPONENTIATION (Modular exponentiation)
-
-        x^n = (x^2)^(n/2) if n is even (par)    2^4 = (2^2)^(4/2) = 4^2 = 16
-        // mirando al caso del par, puedo deducir el de impar
-        n^5 = x(x^(n-1)) -> x(x^2)^((n-1)/2) if n is odd (impar)
-    */
-
     double myPow(double x, int n) {
-        if (n == 0) return 1;
-        if (n < 0) {
-            return myPow(1/x, -n);
-        }
-        if (n % 2 == 0) {
-            return myPow(x*x, n/2);
-        } else {
-            return x * myPow(x*x, (n-1)/2);
-        }
+        return powUtil(x, n);
     }
+    
+    // x^n = (x^2)^(n/2)   -- Exponentiation by squaring   https://wikimedia.org/api/rest_v1/media/math/render/svg/46fe9e68c70c04df4c3d22c469a57d4655b50539
+    // even case -> (x^2)^(n/2)
+    // odd case -> x * (x^2)^(n/2)
+    
+    double powUtil(double x, long n) {
+        if (n == 0) return 1.0;
+        if (n == 1) return x;
+        if (n < 0) {
+            return powUtil(1/x, -n);
+        }
+        double result = powUtil(x*x, n/2);
+        if (n % 2) result *= x;
+        return result;
+    }
+    // log n
 };
