1fd7e8a61c5cad915da7b9f0c57ab75dfe8a7c73
Pablo Pereira <invict1@outlook.es>
2021-08-26 00:13:09 -0300

    .

--- Exercises - LeetCode/1048.cpp
+++ Exercises - LeetCode/1048.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     int longestStrChain(vector<string>& words) {
-       sort(words.begin(), words.end(), [](string &a, string &b) {return a.length() < b.length();});
-
-        int res = 0;
+        sort(words.begin(), words.end(), [](string &a, string& b) { return a.length() < b.length(); });
+        
+        int longestWordSize = 0;
         unordered_map<string, int> dp;
-        for (auto & t : words) {
-            for (int i = 0; i < t.length(); i++) {
-                string pre = t.substr(0, i) + t.substr(i + 1);
-                dp[t] = max(dp[t], dp[pre] + 1);
+        for (auto& word : words) {
+            for (int i = 0; i < word.length(); i++) { // recorro la palabra y me fijo si existe un substr en DP
+                string pre = word.substr(0, i) + word.substr(i + 1);
+                dp[word] = max(dp[word], dp[pre] + 1);
             }
-            res = max(res, dp[t]);
+            longestWordSize = max(longestWordSize, dp[word]);
         }
-        return res;
+        return longestWordSize;
     }
 };
