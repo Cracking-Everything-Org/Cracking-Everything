4cda8da3b6d345d88d0af4e7de03e58c8cfb4274
Pablo Pereira <invict1@outlook.es>
2021-08-25 23:34:18 -0300

    .

--- Exercises - LeetCode/1423.cpp
+++ Exercises - LeetCode/1423.cpp
@@ -5,23 +5,26 @@ public:
         if (cardPoints.size() < k) {
             return -1;
         }
+        
         vector<int> candidates;
         int size = cardPoints.size() - 1;
+        
         for (int i = size - k + 1; i <= size; i++) {
             candidates.push_back(cardPoints[i]);
         }
         for (int i = 0; i < k; i++) {
             candidates.push_back(cardPoints[i]);
         }
+        
         int maxPoints = INT_MIN;
         int points = 0;
-        int startWindow = 0;
-        for (int endWindow = 0; endWindow < candidates.size(); endWindow++) {
-            points += candidates[endWindow];
-            if (endWindow - startWindow + 1 >= k) {
+        int start = 0;
+        for (int end = 0; end < candidates.size(); end++) {
+            points += candidates[end];
+            if (end - start + 1 >= k) {
                 maxPoints = maxPoints < points ? points : maxPoints;
-                points -= candidates[startWindow];
-                startWindow++;
+                points -= candidates[start];
+                start++;
             }
         }
         return maxPoints;
