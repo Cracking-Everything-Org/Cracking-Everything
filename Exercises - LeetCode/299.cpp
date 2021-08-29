acfd1091e997a104565de8fbd12a6fdbd7b2fe01
Pablo Pereira <invict1@outlook.es>
2021-08-29 16:39:38 -0300

    .

--- Exercises - LeetCode/299.cpp
+++ Exercises - LeetCode/299.cpp
@@ -1,38 +1,32 @@
 class Solution {
 public:
     string getHint(string secret, string guess) {
-        string ans = "";
         int bulls = 0;
         int cows = 0;
-        unordered_map<char,int> ht;
-
-        for (int i = 0; i < secret.length(); i++) {
-            if (ht.find(secret[i]) != ht.end()) {
-                ht[secret[i]]++;
-            } else ht[secret[i]] = 1;
-        }
-
+        
+        unordered_map<char, int> candidateCows;
+        
         for (int i = 0; i < secret.length(); i++) {
             if (secret[i] == guess[i]) {
                 bulls++;
-                ht[secret[i]]--;
-                if (ht[secret[i]] == 0) ht.erase(secret[i]);
+            } else {
+                candidateCows[secret[i]]++;
             }
         }
-        for (int i = 0; i < secret.length(); i++) {
-            if (secret[i] != guess[i] && ht.find(guess[i]) != ht.end()) {
-                cows++;
-                ht[guess[i]]--;
-                if (ht[guess[i]] == 0) ht.erase(guess[i]);
+        
+        for (int i = 0; i < guess.length(); i++) {
+            if (secret[i] != guess[i]) {
+                if (candidateCows.find(guess[i]) != candidateCows.end()) {
+                    cows++;
+                    candidateCows[guess[i]]--;
+                    if (candidateCows[guess[i]] == 0) {
+                        candidateCows.erase(guess[i]);
+                    }
+                }
             }
         }
-        if (bulls > 0) {
-            ans += to_string(bulls) + "A";
-        } else ans = "0A";
-
-        if (cows > 0) {
-            ans += to_string(cows) + "B";
-        } else ans += "0B";
-        return ans;
+        
+        string result = to_string(bulls) + "A" + to_string(cows) + "B";
+        return result;
     }
 };
