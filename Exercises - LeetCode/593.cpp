f346ba8f0b9980bd7a81962aa3ec42c0eb5e6c65
Pablo Pereira <invict1@outlook.es>
2021-08-29 22:23:00 -0300

    .

--- Exercises - LeetCode/593.cpp
+++ Exercises - LeetCode/593.cpp
@@ -7,16 +7,16 @@ public:
     }
 
     bool validSquare(vector<int>& p1, vector<int>& p2, vector<int>& p3, vector<int>& p4) {
-        vector<vector<int>> points = {p1, p2, p3, p4};
-        set<double> st;
-        for(int i = 0; i < 4; i++) {
-            for(int j = i + 1; j < 4; j++) {
+        vector<vector<int>> points = { p1, p2, p3, p4 };
+        set<double> distances;
+        for (int i = 0; i < 4; i++) {
+            for (int j = i + 1; j < 4; j++) {
                 double distance = dist(points[i], points[j]);
                 if (distance == 0) return false;
-                st.insert(distance);
+                distances.insert(distance);
             }
         }
-
-        return st.size() == 2;
+        // distancia de una arista y de una diagonal
+        return distances.size() == 2;
     }
 };
