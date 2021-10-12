9d20b346b499168e1d29b69c9611dd05ca1c86b7
Pablo Pereira <invict1@outlook.es>
2021-10-12 17:25:04 -0300

    .

--- Exercises - LeetCode/217.cpp
+++ Exercises - LeetCode/217.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_set<int> hs;
-        for (int i = 0; i < nums.size(); i++) {
-            if (hs.find(nums[i]) != hs.end()) return true;
-            else hs.insert(nums[i]);
+        unordered_set<int> frequency;
+        for (auto num : nums) {
+            if (frequency.find(num) != frequency.end()) return true;
+            frequency.insert(num);
         }
         return false;
     }
--- Exercises - LeetCode/238.cpp
+++ Exercises - LeetCode/238.cpp
@@ -1,76 +1,21 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        vector<int> answer(nums.size());
-
-        answer[0] = 1;
+        vector<int> left(nums.size(), 0);
+        left[0] = 1;
         for (int i = 1; i < nums.size(); i++) {
-            answer[i] = nums[i-1] * answer[i-1];
-        }
-
-        int R = 1;
-        for (int j = nums.size() - 1; j >= 0; j--) {
-            answer[j] = answer[j] * R;
-            R *= nums[j];
-        }
-        return answer;
-    }
-};
-
-
-class Solution {
-public:
-    vector<int> productExceptSelf(vector<int>& nums) {
-        vector<int> prods(nums.size(),0);
-        int leftProd = 1;
-        int rightProd = 1;
-        int countZeros = 0;
-
-        int rPWZ = 1;
-
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] == 0) {
-                countZeros++;
-                if (countZeros == 2) {
-                    return prods;
-                }
-            } else rPWZ *= nums[i];
-            rightProd *= nums[i];
-        }
-
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] == 0) {
-                prods[i] = rPWZ;
-            } else {
-                rightProd /= nums[i];
-                prods[i] = rightProd * leftProd;
-                leftProd *= nums[i];
-            }
-        }
-        return prods;
-    }
-};
-
-class Solution {
-public:
-    vector<int> productExceptSelf(vector<int>& nums) {
-        vector<int> result(nums.size(), 0);
-        if (!nums.size()) return result;
-        
-        vector<int> fromLeft(nums.size(), 0);
-        fromLeft[0] = 1;
-        for (int i = 1; i < nums.size(); i++) {
-            fromLeft[i] = fromLeft[i - 1] * nums[i - 1];
+            left[i] = left[i - 1] * nums[i - 1];
         }
         
-        vector<int> fromRight(nums.size(), 0);
-        fromRight[nums.size() - 1] = 1;
+        vector<int> right(nums.size(), 0);
+        right[nums.size() - 1] = 1;
         for (int i = nums.size() - 2; i >= 0; i--) {
-            fromRight[i] = fromRight[i + 1] * nums[i + 1];
+            right[i] = right[i + 1] * nums[i + 1];
         }
         
+        vector<int> result(nums.size(), 0);
         for (int i = 0; i < nums.size(); i++) {
-            result[i] = fromLeft[i] * fromRight[i];
+            result[i] = left[i] * right[i];
         }
         
         return result;
