4602aac68de10b968f1b97206fbbaf68da250184
Pablo Pereira <invict1@outlook.es>
2021-12-18 00:11:59 -0300

    .

---
+++ New Year Gift/252. Meeting Rooms.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool canAttendMeetings(vector<vector<int>>& intervals) {
+        if (intervals.size() < 2) return true;
+        sort(intervals.begin(), intervals.end());
+        
+        for (int i = 0; i < intervals.size() - 1; i++) {
+            if (intervals[i][1] > intervals[i + 1][0]) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+};
